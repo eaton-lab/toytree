db8cdea03334c952e3836fc7eeb55587e905fdc6
deren <deren.eaton@yale.edu>
2017-04-10 17:15:07 -0400

    installation edits

--- setup.py
+++ setup.py
@@ -39,9 +39,8 @@ setup(
     entry_points={},
     license='GPL',
     classifiers=[
-        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',        
     ],
 )
 
