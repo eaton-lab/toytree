a087f1264d0ef79307277941bfc06a0c10c691d7
deren <deren.eaton@yale.edu>
2017-04-13 01:16:00 -0400

    still meessing with conda recipe

--- conda-recipe/toyplot/build.sh
+++ conda-recipe/toyplot/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-pip install arrow
-pip install behave
-pip install coverage
-pip install coveralls
+#pip install arrow
+#pip install behave
+#pip install coverage
+#pip install coveralls
 #pip install .
-$PYTHON setup.py install  
-
+$PYTHON setup.py install --single-version-externally-managed --record record.txt  
