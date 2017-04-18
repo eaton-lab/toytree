717af076f55e0623afd6aa86a97a2b998b6ed7df
deren <deren.eaton@yale.edu>
2017-04-18 10:39:35 -0400

    changed all node values to scatterplot + text. Changed hide_root values arg to show_root

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -5,5 +5,7 @@ __author__ = "Deren Eaton"
 
 
 from tree import Toytree as tree
+from tree import RandomTree as rtree
+from multitree import MultiTree as multitree
 #from Tree import TreeNode
 #import newick
