243e68ad1789e5384720119a395606614db2137d
deren <deren.eaton@yale.edu>
2017-04-27 18:38:02 -0400

    cosmetic

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -5,8 +5,9 @@ __author__ = "Deren Eaton"
 
 
 from tree import Toytree as tree
-#from tree import RandomTree as rtree
 from tree import randomtree as rtree
 from multitree import MultiTree as multitree
+
+#from tree import RandomTree as rtree
 #from Tree import TreeNode
 #import newick
