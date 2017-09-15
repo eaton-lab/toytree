1874a5b5e7abf6dd94c916328e7db7d61a1a5561
Zachary Sailer <zachsailer@gmail.com>
2017-09-15 08:55:24 -0700

    fix importing errors in python 3

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -4,9 +4,9 @@ __version__ = "0.1.3"
 __author__ = "Deren Eaton"
 
 
-from tree import Toytree as tree
-from tree import randomtree as rtree
-from multitree import MultiTree as multitree
+from .tree import Toytree as tree
+from .tree import randomtree as rtree
+from .multitree import MultiTree as multitree
 
 #from tree import RandomTree as rtree
 #from Tree import TreeNode
