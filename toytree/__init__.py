43a593bc250db13a034a2fd506f6b33cacdbb203
eaton-lab <de2356@columbia.edu>
2018-10-08 17:35:20 -0400

    new init for new structure

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __author__ = "Deren Eaton"
 
+from .Toytree import Toytree as tree
+from .utils import RandomTree as rtree
+from .Multitree import MultiTree as mtree
+from .TreeStyle import COLORS as colors
 
-from .tree import Toytree as tree
-from .tree import RandomTree as rtree
-from .multitree import MultiTree as multitree
-from .style import COLORS as colors
+#from .multitree import MultiTree as multitree
+#from .tree import Toytree as tree
+#from .rtree import RandomTree as rtree
+#from .style import COLORS as colors
 # from .tree import 
