845e6a830f63db5bc24b93f98099c8be62870d70
eaton-lab <de2356@columbia.edu>
2018-10-12 14:11:23 -0400

    randomtree tip labels match idx and started some more mod functions

--- toytree/utils.py
+++ toytree/utils.py
@@ -31,7 +31,7 @@ class TreeMod:
     def __init__(self, ttree):
         self._ttree = ttree
 
-    def scale_root_height(self, treeheight=1):
+    def node_scale_root_height(self, treeheight=1):
         """
         Returns a toytree copy with all nodes scaled so that the root 
         height equals the value entered for treeheight.
@@ -96,6 +96,89 @@ class TreeMod:
         return ctree
 
 
+#     def move_spr(self):
+#         """
+#         Sub-tree pruning and Regrafting. 
+#         Select one edge randomly from the tree and split on that edge to create
+#         two subtrees. Attach one of the subtrees (e.g., the smaller one) 
+#         randomly to the larger tree to create a new node.
+#         ... does SPR break edges connected to root when tree is real rooted?
+#         """
+#         pass
+#         # On rooted trees we can work with nodes easier than edges. Start by
+#         # selected a node at random that is not root.
+#         # nodes = [i for i in self.ttree.tree.traverse() if not i.is_root()]
+#         # rnode = nodes[random.randint(0, len(nodes) - 1)]
+#         # # get all edges on the tree, skip last one which is non-real root edge
+#         # edges = self.ttree.tree.get_edges()[:-1]
+#         # # select a random edge
+#         # redge = edges[random.randint(0, len(edges))]
+#         # # break into subtrees
+#         # tre1 = self.tree.prune(self.tree.get_common_ancestor(redge[0]).idx)
+#         # tre2 = self.tree.prune(self.tree.get_common_ancestor(redge[1]).idx)
+
+
+
+#     def move_tbr(self):
+#         pass
+
+
+#     def move_nni(self):
+#         pass
+
+
+#     def non_parametric_rate_smoothing(self):
+#         """
+#         Non-parametric rate smooting.
+#         A method for estimating divergence times when evolutionary rates are 
+#         variable across lineages by minimizing ancestor-descendant local rate
+#         changes. According to Sanderson this method is motivated by the 
+#         likelihood that evolutionary rates are autocorrelated in time.
+
+#         Reference: Sanderson, M. J. 1997. “A Nonparametric Approach to 
+#         Estimating Divergence Times in the Absence of Rate Constancy.” 
+#         Molecular Biology and Evolution 14 (12): 1218–1218. 
+#         https://doi.org/10.1093/oxfordjournals.molbev.a025731.
+
+#         returns Toytree
+#         """
+#         # p is a fixed exponent
+#         p = 2
+#         W = []
+#         for node in self.ttree.traverse():
+#             if not node.is_leaf():
+#                 children = node.children
+#                 ks = []
+#                 for child in children:
+#                     dist = abs(node.dist - child.dist)
+#                     ks.append(dist ** p)
+#                 W.append(sum(ks))
+
+#         # root rate is mean of all descendant rates -- 
+#         # n is the number of edges (rates) (nnodes - 1 for root)
+#         r_root = np.mean(W)
+#         rootw = []
+#         for child in self.ttree.tree.children:
+#             rootw.append((r_rroot - child.dist) ** p)
+#         w_root = sum(rootw)
+#         W.append(w_root)
+#         k = []
+#         for 
+#         k = sum(  np.exp(abs(ri - rj), p)  )
+#         W = sum(k)
+
+
+# def wfunc(ttree, p):
+#     ws = []
+#     for node in ttree.tree.traverse():
+#         if not node.is_leaf():          
+#             w = sum([(node.dist - child.dist) ** p for child in node.children])
+#             ws.append(w)
+#     return sum(ws)
+
+
+
+
 
 #######################################################
 # Random Tree generation Class
@@ -146,17 +229,17 @@ class RandomTree:
             for node in self.tree.traverse():
                 node.dist /= (2. * ne)
 
-        ## ensure tips are at zero
+        # ensure tips are at zero
         for node in self.tree.traverse():
             if node.is_leaf():
                 node.dist += node.height
 
         # set tipnames, decompose will fill internals
-        ntip = 0
+        ntip = self.ntips - 1
         for tip in self.get_tip_labels():
             node = self.tree.search_nodes(name=tip)[0]
             node.name = "r{}".format(ntip)
-            ntip += 1
+            ntip -= 1
 
         # decompose fills in internal node names and idx
         self._coords.update()
