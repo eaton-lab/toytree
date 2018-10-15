9d4d175de7c7ec7cfa58271eef9f46ddd6a1b78e
eaton-lab <de2356@columbia.edu>
2018-10-14 22:55:50 -0400

    py 2 and 3 generic

--- setup.py
+++ setup.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import glob
 import re
 
-
-#def requires():
-#    """ gets packages from requirements.txt """
-#    with open('requirements.txt') as infile:
-#        return infile.read().splitlines()
-
-
-#def dependency_links():
-#    """ return: the package specifications """
-#    with open('constraints.txt') as infile:
-#        return infile.read().splitlines()
-
-
-## Auto-update ipyrad version from git repo tag
-# Fetch version from git tags, and write to version.py.
-# Also, when git is not available (PyPi package), use stored version.py.
+# get version from __init__.py
 INITFILE = "toytree/__init__.py"
 CUR_VERSION = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                     open(INITFILE, "r").read(),
                     re.M).group(1)
 
+# run setup
 setup(
     name="toytree",
     version=CUR_VERSION,
@@ -34,14 +19,15 @@ setup(
     description="minimalist tree plotting using toyplot",
     long_description=open('README.rst').read(),
     packages=find_packages(),
-    #install_requires=requires(),
-    #dependencies=dependency_links(),
+    install_requires=[
+        "toyplot",
+        "numpy>=1.8.0",
+        "requests",
+    ],
     entry_points={},
     license='GPL',
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
     ],
 )
-
