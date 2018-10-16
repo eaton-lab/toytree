3e60609df2f8a48e80ff1e58c9dc7945dd905ed9
eaton-lab <de2356@columbia.edu>
2018-10-16 14:11:37 -0400

    version to 0.1.16

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-__version__ = "0.2.0"
+__version__ = "0.1.16"
 __author__ = "Deren Eaton"
 
 from .Toytree import Toytree as tree
 from .utils import RandomTree as rtree
 from .Multitree import MultiTree as mtree
 from .TreeStyle import COLORS as colors
-
-#from .multitree import MultiTree as multitree
-#from .tree import Toytree as tree
-#from .rtree import RandomTree as rtree
-#from .style import COLORS as colors
-# from .tree import 
