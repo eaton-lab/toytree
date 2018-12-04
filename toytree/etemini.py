ee9806d32a9168b76fcff9fa85828ea91ee8e70b
eaton-lab <de2356@columbia.edu>
2018-12-04 11:32:03 -0500

    cosmetic

--- toytree/etemini.py
+++ toytree/etemini.py
@@ -114,6 +114,7 @@ class TreeNode(object):
         _root = self.get_tree_root()
         _treeheight = _root.get_distance(_root.get_farthest_leaf()[0])
         return _treeheight - _root.get_distance(self)
+    # TODO: setting height should change the .dist values...
     @height.setter
     def height(self, value):
         try:
@@ -2056,82 +2057,6 @@ class TreeNode(object):
             _resolve(n)
 
 
-    # @staticmethod
-    # def from_parent_child_table(parent_child_table):
-    #     """Converts a parent-child table into an ETE Tree instance. 
-
-    #     >>> tree = Tree.from_parent_child_table([("A", "B", 0.1), ("A", "C", 0.2), ("C", "D", 1), ("C", "E", 1.5)])
-    #     >>> print tree
-    #     """
-    #     def get_node(nodename, dist=None):
-    #         if nodename not in nodes_by_name:
-    #             nodes_by_name[nodename] = Tree(name=nodename, dist=dist)
-    #         node = nodes_by_name[nodename]
-    #         if dist is not None:
-    #             node.dist = dist
-    #         node.name = nodename
-    #         return nodes_by_name[nodename]
-    
-    #     nodes_by_name = {}
-    #     for columns in parent_child_table:
-    #         if len(columns) == 3:
-    #             parent_name, child_name, distance = columns
-    #             dist = float(distance)
-    #         else:
-    #             parent_name, child_name = columns
-    #             dist = None
-    #         parent = get_node(parent_name)
-    #         parent.add_child(get_node(child_name, dist=dist))
-    
-    #     root = parent.get_tree_root()
-    #     return root
-
-        
-    # @staticmethod
-    # def from_skbio(skbio_tree, map_attributes=None):
-    #     """Converts a scikit-bio TreeNode object into ETE Tree object. 
-        
-    #     :argument skbio_tree: a scikit bio TreeNode instance 
-
-    #     :argument None map_attributes: A list of attribute nanes in the
-    #        scikit-bio tree that should be mapped into the ETE tree
-    #        instance. (name, id and branch length are always mapped)
-
-    #     :returns: A new Tree instance 
-
-    #     :example:
-    #     >>> tree = Tree.from_skibio(skbioTree, map_attributes=["value"])
-    #     """        
-    #     from skbio import TreeNode as skbioTreeNode
-        
-    #     def get_ete_node(skbio_node):
-    #         ete_node = all_nodes.get(skbio_node, Tree())
-    #         if skbio_node.length is not None:
-    #             ete_node.dist = float(skbio_node.length)
-    #         ete_node.name = skbio_node.name            
-    #         ete_node.add_features(id=skbio_node.id)
-    #         if map_attributes:
-    #             for a in map_attributes:
-    #                 ete_node.add_feature(a, getattr(skbio_node, a, None))
-    #         return ete_node
-        
-    #     all_nodes = {}
-    #     if isinstance(skbio_tree, skbioTreeNode):
-    #         for node in skbio_tree.preorder(include_self=True):
-    #             all_nodes[node] = get_ete_node(node)
-    #             ete_node = all_nodes[node]
-    #             for ch in node.children:
-    #                 ete_ch = get_ete_node(ch)
-    #                 ete_node.add_child(ete_ch)
-    #                 all_nodes[ch] = ete_ch
-    #         return ete_ch.get_tree_root()
-        
-
-    #def phonehome(self):
-    #    from .. import _ph
-    #    _ph.call()
-        
-
 ## alias
 Tree = TreeNode        
 
@@ -2164,5 +2089,3 @@ def _translate_nodes(root, *nodes):
         return valid_nodes[0]
     else:
         return valid_nodes
-
-
