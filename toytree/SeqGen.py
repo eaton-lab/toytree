dd6bf234a18e438b1e0e59d6a68afbc55973c1bf
eaton-lab <de2356@columbia.edu>
2019-05-04 18:45:37 -0400

    beta testing seqgen submodule

--- toytree/SeqGen.py
+++ toytree/SeqGen.py
@@ -13,19 +13,32 @@ except ImportError:
     )
 
 
+"""
+From the seq-gen paper:
+Seq-Gen reads in a tree from a file containing one or more
+trees in PHYLIP format (Felsenstein, 1993). These trees may
+be rooted or unrooted, but they must include branch lengths.
+Each branch length is assumed to denote the mean number
+of nucleotide substitutions per site that will be simulated
+along that branch. If this is not the case, then a scaling factor
+must be supplied that will transform the branch lengths into
+the mean number of substitutions per site. For example, if the
+branch lengths are given in units of millions of years (Myr)
+and the rate of molecular evolution was 0.5 substitutions
+per site per Myr, then the user should give a scaling factor
+of 0.5 to obtain the expected amount of substitution along
+each branch.
+"""
 
-#######################################################
-# Seqgen simple class
-#######################################################
-FREQS = {
-    "C": 0.25, "A": 0.25, "T": 0.25, "G": 0.25,
-}
+
+# GLOBALS, using alphabetical order ACGT througout
+FREQS = (0.25, 0.25, 0.25, 0.25)
 
 SEQMODEL = {
     "JC": {},                                                     # JC69
-    "K8": {"tstv": 0.5},                                          # K80, K2P
-    "F8": {"freqs": FREQS},                                       # F81
-    "HK": {"tstv": 0.5, "freqs": FREQS},                          # HKY85
+    "K8": {"tstv": 1.0},                                          # K80, K2P
+    "F8": {"pi": FREQS},                                          # F81
+    "HK": {"tstv": 1.0, "pi": FREQS},                             # HKY85
     # "GTR": {"AC": 0.15, "AG": 0.15, "AT": 0.15, 
     # "CG": 0.15, "CT": 0.15, "GT": 0.15}
 }
@@ -58,8 +71,10 @@ class SeqGen(object):
 
         # store X
         np.random.seed(seed)
-        self.tree = ToyTree(tree)
-        self.matrix = np.zeros((len(tree), 1), dtype=bytes)
+        if isinstance(tree, ToyTree):
+            self.tree = tree
+        else:
+            self.tree = ToyTree(tree)
         self.seqdict = {}
 
         # substitution model params
@@ -75,35 +90,36 @@ class SeqGen(object):
                 .format(self.model, ignored))
 
         # equilibrium base frequencies, if not freqs param then set to 0.25
-        self.freqs = FREQS
-        if "freqs" in self.model_kwargs:
-            self.freqs.update(self.model_kwargs["freqs"])
-
-        # order freqs alphabetically and ensure they sum to 1
-        self.pi = np.array([self.freqs[i] for i in "ACGT"], dtype=np.float64)
+        self.pi = np.array(FREQS)
+        if "pi" in self.model_kwargs:
+            self.pi = np.array(self.model_kwargs["pi"])
         assert np.isclose(1.0, sum(self.pi)), "freqs must sum to 1."
 
 
-    def mutate(self, nsites=1):
+    def mutate(self, nsites=1, mode=0):
+        """
+        Simulate sequences.
 
-        # sequence at the ancestral node drawn from equilibrium freqs
-        self.seqdict = {
-            self.tree.treenode.idx: 
-            np.random.choice((0, 1, 2, 3), size=nsites, p=self.pi)
-        }
+        Parameters:
+        -----------
+        mode (int):
+            0: return dictionary mapping tip names to string sequences. 
+            1. return array with rows ordered by tip idx and bases as ints.
+            2. return array with  as 1 but return only SNPs.
+        """
 
         # set alpha=beta if not tstv
-        alpha = 1.0
-        beta = (
+        ts = (
             self.model_kwargs["tstv"] if self.model_kwargs.get("tstv") else 1.0
         )
+        tv = 1.0
 
         # get transition matrix
         self.w = np.array([
-            [0., beta, alpha, beta],
-            [beta, 0., beta, alpha],
-            [alpha, beta, 0., beta],
-            [beta, alpha, beta, 0.],
+            [0., tv, ts, tv],
+            [tv, 0., tv, ts],
+            [ts, tv, 0., tv],
+            [tv, ts, tv, 0.],
         ])
 
         # fill diagonal of transition matrix to sum to 1
@@ -112,31 +128,49 @@ class SeqGen(object):
         # multiply transition matrix by starting frequencies.
         self.qmatrix = self.w * self.pi
 
-        # traverse tree starting with allele at the root
-        seqs = {}        
-        nsites = len(self.seqdict[self.tree.treenode.idx])
-        for node in self.tree.treenode.traverse("preorder"):
-            if not node.is_root():
-                # get ancestral seq for this edge
-                anc = self.seqdict[node.up.idx]
+        # wrap in while loop to repeat if no SNPs found for mode=2
+        while 1:
+            seqs = {}
+            sarr = np.zeros((len(self.tree), nsites), dtype=np.uint8)
+            self.seqdict = {
+                self.tree.treenode.idx: 
+                np.random.choice((0, 1, 2, 3), size=nsites, p=self.pi)
+            }
 
-                # get transition probs for this edge
-                probs = scipy.linalg.expm(self.qmatrix * node.dist)
+            # traverse tree starting with allele at the root
+            for node in self.tree.treenode.traverse("preorder"):
+                if not node.is_root():
+                    # get ancestral seq for this edge
+                    anc = self.seqdict[node.up.idx]
 
-                for idx in range(len(anc)):
+                    # get transition probs for this edge
+                    probs = scipy.linalg.expm(self.qmatrix * node.dist)
 
-                    # current state
-                    state = anc[idx]
+                    # could JIT this loop for max speed
+                    for idx in range(len(anc)):
 
-                    # probability matrix for this site on this edge
-                    trans = probs[state]
-                    new = np.argmax(np.random.multinomial(1, pvals=trans))
-                    anc[idx] = new
+                        # current state
+                        state = anc[idx]
 
-                # store this sequence at this node
-                self.seqdict[node.idx] = anc.copy()
+                        # probability matrix for this site on this edge
+                        trans = probs[state]
+                        new = np.argmax(np.random.multinomial(1, pvals=trans))
+                        anc[idx] = new
 
-                # store final result
-                if node.is_leaf():
-                    seqs[node.name] = "".join([BASEDICT[i] for i in anc])               
-        return seqs
+                    # store this sequence at this node
+                    self.seqdict[node.idx] = anc.copy()
+
+                    # store final result
+                    if node.is_leaf():
+                        seqs[node.name] = "".join([BASEDICT[i] for i in anc])
+                        sarr[node.idx] = anc
+
+            # return results
+            if mode == 0:
+                return seqs
+            elif mode == 1:
+                return sarr
+            else:
+                snps = sarr[:, np.any(sarr != sarr[0], axis=0)]
+                if snps.size:
+                    return snps[:, 0]
