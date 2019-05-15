92001abe9965148d028a0102a02c9e4c0cfe536a
eaton-lab <de2356@columbia.edu>
2019-05-15 12:38:23 -0400

    moved NW_FORMATS and some other shared to utils

--- toytree/utils.py
+++ toytree/utils.py
@@ -2,10 +2,9 @@
 
 from __future__ import print_function, division, absolute_import
 
-from .etemini import TreeNode
-import toytree
-import random         # b/c ete uses random seed.
+import random 
 import re
+import toytree
 
 
 #######################################################
@@ -14,6 +13,114 @@ import re
 class ToytreeError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
+
+
+class TreeError(Exception):
+    "A problem occurred during a TreeNode operation"
+    def __init__(self, value=''):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+# TREE FORMATS
+NW_FORMAT = {
+    # flexible with support
+    # Format 0 = (A:0.35,(B:0.72,(D:0.60,G:0.12)1.00:0.64)1.00:0.56);
+    0: [
+        ('name', str, True),
+        ('dist', float, True),
+        ('support', float, True),
+        ('dist', float, True),
+    ],
+
+    # flexible with internal node names
+    # Format 1 = (A:0.35,(B:0.72,(D:0.60,G:0.12)E:0.64)C:0.56);
+    1: [
+        ('name', str, True),
+        ('dist', float, True),
+        ('name', str, True),
+        ('dist', float, True),      
+    ],
+
+    # strict with support values
+    # Format 2 = (A:0.35,(B:0.72,(D:0.60,G:0.12)1.00:0.64)1.00:0.56);
+    2: [
+        ('name', str, False),
+        ('dist', float, False),
+        ('support', str, False),
+        ('dist', float, False),      
+    ],
+
+    # strict with internal node names
+    # Format 3 = (A:0.35,(B:0.72,(D:0.60,G:0.12)E:0.64)C:0.56);
+    3: [
+        ('name', str, False),
+        ('dist', float, False),
+        ('name', str, False),
+        ('dist', float, False),      
+    ],
+
+    # strict with internal node names
+    # Format 4 = (A:0.35,(B:0.72,(D:0.60,G:0.12)));
+    4: [
+        ('name', str, False),
+        ('dist', float, False),
+        (None, None, False),
+        (None, None, False),      
+    ],
+
+    # Format 5 = (A:0.35,(B:0.72,(D:0.60,G:0.12):0.64):0.56);
+    5: [
+        ('name', str, False),
+        ('dist', float, False),
+        (None, None, False),
+        ('dist', float, False),      
+    ],
+
+    # Format 6 = (A:0.35,(B:0.72,(D:0.60,G:0.12)E)C);
+    6: [
+        ('name', str, False),
+        (None, None, False),
+        (None, None, False),        
+        ('dist', float, False),      
+    ],
+
+    # Format 7 = (A,(B,(D,G)E)C);
+    7: [
+        ('name', str, False),
+        ('dist', float, False),
+        ('name', str, False),        
+        (None, None, False),        
+    ],    
+
+
+    # Format 8 = (A,(B,(D,G)));
+    8: [
+        ('name', str, False),
+        (None, None, False),
+        ('name', str, False),        
+        (None, None, False),
+    ],
+
+    # Format 9 = (,(,(,)));
+    9: [
+        ('name', str, False),
+        (None, None, False),
+        (None, None, False),
+        (None, None, False),
+    ],    
+
+    # Format 10 = ((a[&Z=1,Y=2]:1.0[&X=3], b[&Z=1,Y=2]:3.0[&X=2]):1.0[&L=1,W=0], ...
+    # NHX Like mrbayes NEXUS common
+    10: [
+        ('name', str, True),
+        ('dist', str, True),
+        ('name', str, True),
+        ('dist', str, True),
+    ]
+}
+
 
 
 #######################################################
@@ -301,7 +408,7 @@ class RandomTree(object):
         random.seed(seed)
 
         # generate tree with N tips.
-        tmptree = TreeNode()
+        tmptree = toytree.tree().treenode  # TreeNode()
         tmptree.populate(ntips)
         self = toytree.tree(newick=tmptree.write())
 
