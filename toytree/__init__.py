80bf4b9b7fd07f630b540051a64fd9cc514e0ac0
eaton-lab <de2356@columbia.edu>
2019-12-05 01:34:51 -0500

    new rawtree class

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "Deren Eaton"
 
 from .Toytree import ToyTree as tree
+from .Toytree import RawTree as _rawtree
 from .utils import RandomTree as rtree
 from .Multitree import MultiTree as mtree
 from .TreeStyle import COLORS as colors
