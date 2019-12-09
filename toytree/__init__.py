a29d5851b9c0cb4de5271ec16ad7ac108e9f9145
eaton-lab <de2356@columbia.edu>
2019-12-09 18:11:09 -0500

    1.0.7 update

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __author__ = "Deren Eaton"
 
 from .Toytree import ToyTree as tree
 from .Toytree import RawTree as _rawtree
-from .utils import RandomTree as rtree
+from .Randomtree import RandomTree as rtree
 from .Multitree import MultiTree as mtree
-from .TreeStyle import COLORS as colors
+
+
+# make a color palette easily accessible and an iter cycling version
+from .TreeStyle import COLORS1 as colors
+from .TreeStyle import COLORS2 as darkcolors
+import itertools as _itertools
+icolors1 = _itertools.cycle(colors)
+icolors2 = _itertools.cycle(darkcolors)
