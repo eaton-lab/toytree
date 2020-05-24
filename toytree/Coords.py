008a649e1f2cb7f6a4b5ba407a7bb238b38625ca
eaton-lab <de2356@columbia.edu>
2020-05-24 15:04:35 -0400

    removed lines/coords and allow to return tree coords without modifying

--- toytree/Coords.py
+++ toytree/Coords.py
@@ -29,11 +29,6 @@ class Coords:
     verts: ndarray
         2-d array with relative cartesian coordinate positions of nodes (n)
         Rows are ordered by the numbered 'idx' feature of nodes.
-    coords: ndarray
-        2-d array with cartesian coordinates for tips and node crown positions.
-        Rows are ordered postorder idx.
-        Used for drawing edges on phylograms, not for nodes.
-    lines: ndarray
         ...
     """
     def __init__(self, ttree):
@@ -44,11 +39,9 @@ class Coords:
         # the vertices and edge tuples for normal layouts ('n')
         self.edges = None
         self.verts = None
-        self.lines = None
-        self.coords = None
 
 
-    def update(self):
+    def update(self, layout=None):
         """
         Updates cartesian coordinates for drawing tree graph
         """
@@ -60,33 +53,25 @@ class Coords:
             if node.is_leaf():
                 self.ttree.ntips += 1
 
-        # get new shape and clear for attrs
-        self.edges = np.zeros((self.ttree.nnodes - 1, 2), dtype=int)
-        self.verts = np.zeros((self.ttree.nnodes, 2), dtype=float)
-        self.lines = np.zeros(((self.ttree.nnodes * 2) - 2, 2), dtype=float)
-        self.coords = np.zeros(((self.ttree.nnodes * 2) - 1, 2), dtype=float)
-
         # updates idxs and fixed_idx for any tree manipulations
         self.update_idxs()
         self.update_fixed_order()      # in case ntips changed
 
+        # get new shape and clear for attrs
+        self.edges = None  # np.zeros((self.ttree.nnodes - 1, 2), dtype=int)
+        self.verts = None  # np.zeros((self.ttree.nnodes, 2), dtype=float)
+
         # get edges and verts (node locations)
-        if self.ttree.style.layout in ("c", "circ", "circular"):
+        layout = self.ttree.style.layout
+        if layout == 'c':
             self.circ = Circle(self.ttree)
-            self.assign_radial_vertices()
+            self.verts, self.edges = self.get_radial_coords()
+        else:
+            # returns with verts oriented by layout
+            self.verts, self.edges = self.get_linear_coords(layout=layout)
 
-        else:           
-            self.assign_vertices()
-
-        # if unrooted use these vertices as start in a force directed push
-        # if self.ttree.style.layout in ("x", "unrooted"):
-        # self.force_directed_verts()
-
-        # get lines and coords (node neighbor locations for 'p' edges)
-        # self.new_assign_coordinates()      # get edge locations
-        # self.assign_coordinates()            # get edge locations        
-        # self.assign_coordinates()            # get edge locations        
-        self.reorient_coordinates()    # orientation can reorder dimensions
+        # orientation can reorder dimensions
+        # self.verts = self.reorient_coords(self.verts)
 
 
 
@@ -132,14 +117,13 @@ class Coords:
 
 
 
-    def assign_radial_vertices(self):
+    def get_radial_coords(self, use_edge_lengths=True):
         """
         Assign .edges and .verts for node positions in a fan tree.
         The farthest tip aligns at the circumference.
         """
         # shortname 
-        uselen = bool(self.ttree.style.use_edge_lengths)
-        if not uselen:
+        if not use_edge_lengths:
             nbits = self.ttree.treenode.get_farthest_leaf(True)[1]
 
         # use cache to fill edges array 
@@ -155,7 +139,7 @@ class Coords:
                 node.radians = self.circ.tip_radians[idx]
 
                 # get positions of tips using radians and radius   
-                if uselen:
+                if use_edge_lengths:
                     node.radius = self.circ.radius - node.height
                     node.x, node.y = self.circ.get_node_coords(node)
                 else:
@@ -166,7 +150,7 @@ class Coords:
             else:
 
                 # height is either distance or nodes from root
-                if uselen:
+                if use_edge_lengths:
                     node.radius = self.circ.radius - node.height
                 else:
                     node.radius = sum(1 for i in node.iter_ancestors())
@@ -185,20 +169,20 @@ class Coords:
 
 
 
-    def assign_vertices(self):
+    def get_linear_coords(self, layout=None, use_edge_lengths=True):
         """
         Sets .edges, .verts for node positions. 
         X and Y positions here refer to base assumption that tree is right
         facing, reorient_coordinates() will handle re-translating this.        
         """
-        # shortname 
-        uselen = bool(self.ttree.style.use_edge_lengths)
+        edges = np.zeros((self.ttree.nnodes - 1, 2), dtype=int)
+        verts = np.zeros((self.ttree.nnodes, 2), dtype=float)
 
         # use cache to fill edges array 
         for idx in range(self.ttree.nnodes - 1):
             parent = self.ttree.idx_dict[idx].up
             if parent:
-                self.edges[idx, :] = (parent.idx, idx)
+                edges[idx, :] = (parent.idx, idx)
 
         # store verts array with x,y positions of nodes (lengths of branches)
         # we want tips to align at the right face (larger axis number)
@@ -215,7 +199,7 @@ class Coords:
             node = self.ttree.idx_dict[idx]
 
             if node.is_leaf() and (not node.is_root()):
-                if uselen:
+                if use_edge_lengths:
                     toroot = sum(
                         i.dist for i in node.iter_ancestors() 
                         if not i.is_root()
@@ -236,7 +220,7 @@ class Coords:
 
                 # all other trees
                 else:
-                    if uselen:
+                    if use_edge_lengths:
                         node.y = nch[0].y + nch[0].dist
                     else:
                         node.y = max((i.y for i in nch)) + 1
@@ -245,104 +229,101 @@ class Coords:
                     node.x = sum(i.x for i in nch) / float(len(nch))
 
             # store the vertex
-            self.verts[idx] = [node.x, node.y]
+            verts[idx] = [node.x, node.y]
 
         # scale so that node idx 0 (or fixed_order x) is at (0, 0)
-        if uselen:
-            adjust = self.verts[:, 1].min()
-            self.verts[:, 1] -= adjust
+        if use_edge_lengths:
+            adjust = verts[:, 1].min()
+            verts[:, 1] -= adjust
+
+        # orient for layout
+        verts = self.reorient_coords(verts, layout)
+        return verts, edges
 
 
 
-    # def assign_coordinates(self):
-    #     """
-    #     coords and lines allow 'p' type edges (not all straight lines)
-    #     """
-    #     for idx in range(self.ttree.nnodes):
-    #         node = self.ttree.idx_dict[idx]
-
-    #         # store coord of this vertex
-    #         self.coords[idx] = (node.x, node.y)
-    #         if not node.is_root():
-
-    #             # store coord of hidden vertex
-    #             nidx = idx + self.ttree.nnodes
-    #             if self.ttree.style.layout == 'c':
-    #                 self.coords[nidx] = self.circ.get_node_lines(node)
-    #             else:
-    #                 self.coords[nidx] = (node.x, node.up.y)
-
-    #             # store line connecting node to hidden vertex
-    #             self.lines[idx] = (idx, nidx)
-
-    #             # store line connecting hidden vertex to node parent
-    #             self.lines[nidx - 1] = (nidx, node.up.idx)
-
-
-
-    def reorient_coordinates(self):
+    def reorient_coords(self, verts, layout=None):
         """
         Returns a modified .verts array with new coordinates for nodes. 
         This does not need to modify .edges. The order of nodes, and therefore
         of verts rows is still the same because it is still based on the tree
         branching order (ladderized usually). 
         """
+
+        # select layout
+        if layout is None:
+            layout = self.ttree.style.layout
+
         # if tree is empty then bail out
         if len(self.ttree) < 2:
-            return
+            return verts
 
-        # TODO: rotate? arc?
-        if self.ttree.style.layout in ('c', 'circular', 'x', 'unrooted'):
-            return 
+        # this is done later in drawing.
+        if layout == 'c':
+            return verts
 
         # default: Down-facing tips align at y=0, first ladderized tip at x=0
-        if self.ttree.style.layout in ('d', 'down'):
-            pass
+        elif layout == 'd':
+            return verts
 
         # right-facing tips align at x=0, last ladderized tip at y=0
-        elif self.ttree.style.layout in ('r', 'right'):
+        elif layout == 'r':
 
             # verts swap x and ys and make xs 0 to negative
-            tmp = np.zeros(self.verts.shape)
-            tmp[:, 1] = self.verts[:, 0]
-            tmp[:, 0] = self.verts[:, 1] * -1
-            self.verts = tmp
+            tmp = np.zeros(verts.shape)
+            tmp[:, 1] = verts[:, 0]
+            tmp[:, 0] = verts[:, 1] * -1
+            return tmp
 
-            # coords...
-            tmp = np.zeros(self.coords.shape)
-            tmp[:, 1] = self.coords[:, 0]
-            tmp[:, 0] = self.coords[:, 1] * -1
-            self.coords = tmp
-
-        elif self.ttree.style.layout in ('l', 'left'):
+        elif layout == 'l':
 
             # verts swap x and ys and make xs 0 to negative
-            tmp = np.zeros(self.verts.shape)
-            tmp[:, 1] = self.verts[:, 0]
-            tmp[:, 0] = self.verts[:, 1] 
-            self.verts = tmp
+            tmp = np.zeros(verts.shape)
+            tmp[:, 1] = verts[:, 0]
+            tmp[:, 0] = verts[:, 1] 
+            return tmp
 
-            # coords...
-            tmp = np.zeros(self.coords.shape)
-            tmp[:, 1] = self.coords[:, 0]
-            tmp[:, 0] = self.coords[:, 1]
-            self.coords = tmp
-
-        elif self.ttree.style.layout in ('u', 'up'):
+        elif layout == 'u':
             # verts swap x and ys and make xs 0 to negative
-            tmp = np.zeros(self.verts.shape)
-            tmp[:, 1] = self.verts[:, 1] * -1
-            tmp[:, 0] = self.verts[:, 0] * -1
-            self.verts = tmp
-
-            # coords...
-            tmp = np.zeros(self.coords.shape)
-            tmp[:, 1] = self.coords[:, 1] * -1
-            tmp[:, 0] = self.coords[:, 0] * -1
-            self.coords = tmp
+            tmp = np.zeros(verts.shape)
+            tmp[:, 1] = verts[:, 1] * -1
+            tmp[:, 0] = verts[:, 0] * -1
+            return tmp
 
         else:
             raise ToytreeError("layout not recognized")
+
+
+
+    def get_radii(self, layout):
+        """
+        radius of every node relative to 0,0 origin.
+        """
+
+        # custom request layout
+        if layout is None:
+            layout = self.ttree.layout
+
+        # get circular layout
+        if layout == 'c':
+            self.radii = np.array(
+                [self.ttree.idx_dict[i].radius for i in range(self.nnodes)])
+        else:
+            self.radii = np.repeat(0, self.ntips)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
