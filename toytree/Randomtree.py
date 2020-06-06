47977628024c326d211fccf6b02a6f7af32f1e89
eaton-lab <de2356@columbia.edu>
2020-06-05 22:36:53 -0400

    faster tree sims with new copy,update, changes

--- toytree/Randomtree.py
+++ toytree/Randomtree.py
@@ -12,8 +12,156 @@ from .utils import ToytreeError
 
 class RandomTree(object):
 
+
     @staticmethod
-    def coaltree(ntips, ne=None, seed=None):
+    def unittree(ntips, treeheight=1.0, random_names=False, seed=None):
+        """
+        Returns a random tree ultrametric topology w/ N tips and a root 
+        height set to 1 or a user-entered treeheight value. Descendant 
+        nodes are evenly spaced between the root and time 0.
+
+        Parameters
+        -----------
+        ntips (int):
+            The number of tips in the randomly generated tree
+
+        treeheight(float):
+            Scale tree height (all edges) so that root is at this height.
+
+        seed (int):
+            Random number generator seed.
+        """
+        # seed generator
+        random.seed(seed)
+
+        # generate tree with N tips.
+        tmptree = toytree.tree().treenode  # TreeNode()
+        tmptree.populate(ntips)
+        self = toytree.tree(newick=tmptree.write())
+
+        # set tip names by labeling sequentially from 0
+        self = (
+            self
+            .ladderize()
+            .mod.make_ultrametric()
+            .mod.node_scale_root_height(treeheight)
+        )
+
+        # set tipnames randomly (doesn't have to match idx)
+        nidx = list(range(self.ntips))[::-1]
+        if random_names:
+            random.shuffle(nidx)
+        for tidx, node in enumerate(self.treenode.get_leaves()):
+            node.name = "r{}".format(nidx[tidx])
+
+        # set all support values to 100 default
+        for node in self.treenode.traverse():
+            node.support = 100
+
+        # fill internal node names and idxs
+        self.treenode.ladderize()
+        self._coords.update()
+        return self
+
+
+    @staticmethod
+    def imbtree(ntips, treeheight=1.0, random_names=False):
+        """
+        Return an imbalanced (comb-like) tree topology.
+        """
+        node = toytree.TreeNode.TreeNode()
+        node.add_child(name="r0")
+        node.add_child(name="r1")
+
+        for i in range(2, ntips):
+            # empty node
+            cherry = toytree.TreeNode.TreeNode()
+            # add new child
+            cherry.add_child(name="r" + str(i))
+            # add old tree
+            cherry.add_child(node)
+            # update rtree
+            node = cherry
+
+        # get toytree from newick            
+        tre = toytree.tree(node)
+        tre = tre.mod.make_ultrametric(nocopy=True)
+        tre = tre.mod.node_scale_root_height(treeheight, nocopy=True)
+        tre._coords.update()
+
+        # randomize tip names
+        nidx = list(range(tre.ntips))
+        if random_names:
+            random.shuffle(nidx)
+        for idx, node in tre.idx_dict.items():
+            if node.is_leaf():
+                node.name = "r{}".format(nidx[idx])
+        return tre
+
+
+    @staticmethod
+    def baltree(ntips, treeheight=1.0, random_names=False):
+        """
+        Returns a balanced tree topology.
+        """
+        # require even number of tips
+        if ntips % 2:
+            raise ToytreeError("balanced trees must have even number of tips.")
+
+        # make first cherry
+        rtree = toytree.tree()
+        rtree.treenode.add_child(name="r0")
+        rtree.treenode.add_child(name="r1")
+
+        # add tips in a balanced way
+        for i in range(2, ntips):
+
+            # get node to split
+            node = return_small_clade(rtree.treenode)
+
+            # add two children
+            node.add_child(name=node.name)
+            node.add_child(name="r" + str(i))
+
+            # rename ancestral node
+            node.name = None
+
+        # get toytree from newick            
+        tre = toytree.tree(rtree)  # .write(tree_format=9))
+        tre = tre.mod.make_ultrametric().mod.node_scale_root_height(treeheight)
+        tre._coords.update()
+
+        # rename tips so names are in order
+        nidx = list(range(tre.ntips))
+        if random_names:
+            random.shuffle(nidx)
+        for idx, node in tre.idx_dict.items():
+            if node.is_leaf():
+                node.name = "r{}".format(nidx[idx])
+        return tre
+
+
+    @staticmethod
+    def rtree(ntips, treeheight=1.0, random_names=False, seed=None):
+        """
+        Randomly assigns edge lengths between U(0-1) to edges 
+        and then scales total treeheight to 1.0 or entered value.
+        """
+        # get a unit tree
+        self = toytree.rtree.unittree(ntips, treeheight, random_names, seed)
+
+        # randomly assign node dists
+        self = self.set_node_values(
+            "dist", {i: random.random() for i in range(self.nnodes)}
+        )
+
+        # rescale total height to .
+        self = self.mod.node_scale_root_height(treeheight, nocopy=True)
+        return self
+
+
+    @staticmethod
+    def coaltree(ntips, ne=None, random_names=False, seed=None):
         """
         Returns a coalescent tree with ntips samples and waiting times 
         between coalescent events drawn from the kingman coalescent:
@@ -62,10 +210,13 @@ class RandomTree(object):
             if node.is_leaf():
                 node.dist += node.height
 
-        # set tipnames
-        for tip in self.get_tip_labels():
-            node = self.treenode.search_nodes(name=tip)[0]
-            node.name = "r{}".format(node.idx)
+        # set tipnames to r{idx}
+        nidx = list(range(self.ntips))
+        if random_names:
+            random.shuffle(nidx)
+        for idx, node in self.idx_dict.items():
+            if node.is_leaf():
+                node.name = "r{}".format(nidx[idx])
 
         # decompose fills in internal node names and idx
         self._coords.update()
@@ -73,162 +224,25 @@ class RandomTree(object):
 
 
     @staticmethod
-    def unittree(ntips, treeheight=1.0, seed=None):
-        """
-        Returns a random tree ultrametric topology w/ N tips and a root 
-        height set to 1 or a user-entered treeheight value. Descendant 
-        nodes are evenly spaced between the root and time 0.
-
-        Parameters
-        -----------
-        ntips (int):
-            The number of tips in the randomly generated tree
-
-        treeheight(float):
-            Scale tree height (all edges) so that root is at this height.
-
-        seed (int):
-            Random number generator seed.
-        """
-        # seed generator
-        random.seed(seed)
-
-        # generate tree with N tips.
-        tmptree = toytree.tree().treenode  # TreeNode()
-        tmptree.populate(ntips)
-        self = toytree.tree(newick=tmptree.write())
-
-        # set tip names by labeling sequentially from 0
-        self = (
-            self
-            .ladderize()
-            .mod.make_ultrametric()
-            .mod.node_scale_root_height(treeheight)
-        )
-
-        # set tipnames randomly (doesn't have to match idx)
-        nidx = list(range(self.ntips))
-        random.shuffle(nidx)
-        for tidx, node in enumerate(self.treenode.get_leaves()):
-            node.name = "r{}".format(nidx[tidx])
-
-        for node in self.treenode.traverse():
-            node.support = 100            
-        # fill internal node names and idx
-        self.treenode.ladderize()
-        self._coords.update()
-        return self
-
-
-    @staticmethod
-    def imbtree(ntips, treeheight=1.0):
-        """
-        Return an imbalanced (comb-like) tree topology.
-        """
-        node = toytree.TreeNode.TreeNode()
-        node.add_child(name="r0")
-        node.add_child(name="r1")
-
-        for i in range(2, ntips):
-            # empty node
-            cherry = toytree.TreeNode.TreeNode()
-            # add new child
-            cherry.add_child(name="r" + str(i))
-            # add old tree
-            cherry.add_child(node)
-            # update rtree
-            node = cherry
-
-        # get toytree from newick            
-        tre = toytree.tree(node)
-        tre = tre.mod.make_ultrametric(nocopy=True)
-        tre = tre.mod.node_scale_root_height(treeheight, nocopy=True)
-        tre._coords.update()
-        return tre
-
-
-    @staticmethod
-    def baltree(ntips, treeheight=1.0):
-        """
-        Returns a balanced tree topology.
-        """
-        # require even number of tips
-        if ntips % 2:
-            raise ToytreeError("balanced trees must have even number of tips.")
-
-        # make first cherry
-        rtree = toytree.tree()
-        rtree.treenode.add_child(name="r0")
-        rtree.treenode.add_child(name="r1")
-
-        # add tips in a balanced way
-        for i in range(2, ntips):
-
-            # get node to split
-            node = return_small_clade(rtree.treenode)
-
-            # add two children
-            node.add_child(name=node.name)
-            node.add_child(name="r" + str(i))
-
-            # rename ancestral node
-            node.name = None
-
-        # rename tips so names are in order
-        idx = len(rtree) - 1
-        for node in rtree.treenode.traverse("postorder"):
-            if node.is_leaf():
-                node.name = "r" + str(idx)
-                idx -= 1
-
-        # get toytree from newick            
-        tre = toytree.tree(rtree.write(tree_format=9))
-        tre = tre.mod.make_ultrametric()
-        self = tre.mod.node_scale_root_height(treeheight)
-        self._coords.update()
-        return self        
-
-
-    @staticmethod
-    def rtree(ntips, treeheight=1.0, seed=None):
-        """
-        Randomly assigns edge lengths between U(0-1) to edges 
-        and then scales total treeheight to 1.0 or entered value.
-        """
-        # get a unit tree
-        self = toytree.rtree.unittree(ntips, treeheight, seed)      
-
-        # randomly assign node dists
-        self = self.set_node_values(
-            "dist", {i: random.random() for i in range(self.nnodes)}
-        )
-
-        # rescale total height to .
-        self = self.mod.node_scale_root_height(treeheight, nocopy=True)
-        return self        
-
-
-    @staticmethod
-    def bdtree(b=1, d=0, stop="taxa", n=10, t=4, seed=None, verbose=False):
+    def bdtree(ntips, b=1, d=0, maxtime=None, random_names=False, seed=None, verbose=False):
         """
         Generate a classic birth/death tree.
 
         Parameters
         -----------
+        ntips (int):
+            Number of tips to generate for 'taxa' stopping criterion.
+
         b (float):
             Birth rate per time unit
 
         d (float):
             Death rate per time unit (d=0 produces Yule trees)
 
-        stop (str):
-            Stopping critereon. Valid values are only 'taxa' or 'time'.
-
-        n (int):
-            Number of tips to generate for 'taxa' stopping criterion.
-
-        t (float):
-            Amount of time to simulate for 'time' stopping criterion.
+        maxtime (None or int):
+            if maxtime is int then stopping criterion is time and the ntips
+            argument is ignored. Simulations are conditional on tips existing
+            at maxtime, and will reset until a tree reaches maxtime.
 
         seed (int):
             Random number generator seed.
@@ -236,81 +250,149 @@ class RandomTree(object):
         verbose (bool):
             Print some useful information
         """
-        if stop not in ["taxa", "time"]:
-            raise ToytreeError("stop must be either 'taxa' or 'time'")
+        assert b <= 1, "birth rate should be between 0-1"
+        assert d <= 1, "death rate should be between 0-1"
 
+        # set random seed
         if seed:
             np.random.seed(seed)
 
+        # start from random tree (idxs will be re-assigned at end)
         tre = toytree.tree()
+        tre.treenode.idx = 0
+        gidx = 1
 
-        taxa_stop = n
-        time_stop = t
-
-        # Counters for extinctions, total events, and time
+        # counters for extinctions, total events, and time
+        resets = 0
         ext = 0
         evnts = 0
         t = 0
 
-        while(1):
+        # continue until stop var
+        while 1:
+
+            # get current tips
             tips = tre.treenode.get_leaves()
-            # Sample time interval
-            dt = np.random.exponential(1/(len(tips) * (b + d)))
+
+            # sample time until next event, increment t and evnts
+            dt = np.random.exponential(1 / (len(tips) * (b + d)))
             t = t + dt
             evnts += 1
 
+            # sample a [0-1] to choose birth or death and sample a tip node
             r = np.random.sample()
             sp = np.random.choice(tips)
-            if (r <= b/(b + d)):
-                # birth event
-                c1 = sp.add_child(name=str(t)+"-1", dist=0)
-                c1.add_feature("tdiv", t)
-                c2 = sp.add_child(name=str(t)+"-2", dist=0)
-                c2.add_feature("tdiv", t)
-            else:
-                # extinction event
-                sp.delete(preserve_branch_length=True,\
-                            prevent_nondicotomic=False)
-                tre = _prune(tre)
-                ext += 1
 
-            # Update branch lengths
+            # event is a birth
+            if (r <= b / (b + d)):
+                c1 = sp.add_child(name=str(t) + "-1", dist=0)
+                c1.add_feature("tdiv", t)
+                c1.idx = gidx
+                gidx += 1
+
+                c2 = sp.add_child(name=str(t) + "-2", dist=0)
+                c2.add_feature("tdiv", t)
+                c2.idx = gidx
+                gidx += 1
+
+            # else event is extinction
+            else:
+                # get parent node
+                parent = sp.up
+
+                # if no parent then reset to empty tree
+                if parent is None:
+                    resets += 1
+                    tre = toytree.tree()
+                    tre.treenode.idx = 0
+                    gidx = 1
+                    ext = 0
+                    evnts = 0
+                    t = 0
+
+                # connect parent to sp' children
+                else:
+                    # if parent is None then reset
+                    if sp.up is None:
+                        tre = toytree.tree()
+                        tre.treenode.idx = 0
+                        gidx = 1
+                        ext = 0
+                        evnts = 0
+                        t = 0
+
+                    # if parent is root then sister is new root
+                    elif sp.up is tre.treenode:
+                        tre = [i for i in sp.up.children if i != sp][0]
+                        tre = toytree.tree(tre)
+                        tre.up = None
+
+
+                    # if parent is a non-root node then connect sister to up.up
+                    else:
+                        # get sister
+                        sister = [i for i in sp.up.children if i != sp][0]
+
+                        # connect sister to grandparent
+                        sister.up = sp.up.up
+
+                        # drop parent from grandparent
+                        sp.up.up.children.remove(sp.up)
+
+                        # add sister to grandparent children
+                        sp.up.up.children.append(sister)
+
+                        # extend sisters dist to reach grandparent
+                        sister.dist += sp.up.dist
+
+                    ext += 1
+
+            # update branch lengths so all tips end at time=present
             tips = tre.treenode.get_leaves()
             for x in tips:
                 x.dist += dt
 
-            # Check stopping criterion
-            tips = tre.treenode.get_leaves()
-            done = False
-            if stop == "taxa":
-                if len(tips) >= taxa_stop:
-                    done = True
-            elif stop == "time":
-                if t >= time_stop:
-                    done = True
-            elif len(tips) == 0:
-                print("All lineages extinct")
-                done = True
-            if done:
-                if verbose:
-                    print("Birth events {}".format(evnts))
-                    print("Extinctions {} (per birth {})".format(ext, ext/evnts))
-                for i, t in enumerate(tips[::-1]):
-                    t.name = "r{}".format(i)
-                tre._coords.update()
-                return tre
+            # time stopping criterion... not quite working right.
+            if maxtime:
+                if t >= maxtime:
+                    break
+
+            # ntips stopping criterion.
+            else:
+                if len(tips) > ntips:
+                    break
+
+        # report status
+        if verbose:
+            fill = (evnts - ext, ext, evnts / (evnts - ext), resets)
+            print("b:\t{}\nd:\t{}\nb/d:\t{:.2f}\nreset:\t{}".format(*fill))
+
+        # update coords and return
+        tre._coords.update()
+
+        # rename tips so names are in order else random
+        nidx = list(range(tre.ntips))
+        if random_names:
+            random.shuffle(nidx)
+        for idx, node in tre.idx_dict.items():
+            if node.is_leaf():
+                node.name = "r{}".format(nidx[idx])
+        return tre
 
 
 def _prune(tre, verbose=False):
-    "Helper function for recursively pruning extinct branches in bd trees"
+    """
+    Helper function for recursively pruning extinct branches in bd trees.
+    Dynamic func!
+    """
     ttree = tre.copy()
     tips = ttree.treenode.get_leaves()
-    
+
     if np.any(np.array([x.height for x in tips]) > 0):
         for t in tips:
             if not np.isclose(t.height, 0):
-                if verbose: print("Removing node/height {}/{}".format(t.name,\
-                                                                      t.height))
+                if verbose: 
+                    print("Removing node/height {}/{}".format(t.name, t.height))
                 t.delete(prevent_nondicotomic=False)
                 ttree = _prune(ttree)
     return ttree
