481cef449bcb10bb9a406796609a845e98c6b7b9
eaton-lab <de2356@columbia.edu>
2020-06-21 23:32:58 -0400

    removed old fit_tip_labels for new extents, and added Grid setup for mtrees

--- toytree/CanvasSetup.py
+++ toytree/CanvasSetup.py
@@ -16,6 +16,76 @@ ITERABLE = (list, tuple, np.ndarray)
 
 
 
+class GridSetup:
+    """
+    Returns Canvas and Cartesian axes objects to fit a grid of trees.
+    """
+    def __init__(self, nrows, ncols, width, height, layout):
+
+        # style args can include height/width, nrows, ncols, shared,...
+        self.nrows = nrows
+        self.ncols = ncols
+        self.width = width
+        self.height = height
+        self.layout = layout
+
+        # get .canvas and .axes
+        self.get_tree_dims()
+        self.get_canvas_and_axes()
+
+
+    def get_canvas_and_axes(self):
+        """
+        Set .canvas and .axes objects
+        """
+        self.canvas = toyplot.Canvas(
+            height=self.height,
+            width=self.width,
+        )        
+
+        self.axes = [
+            self.canvas.cartesian(
+                grid=(self.nrows, self.ncols, i),
+                padding=10,
+                margin=25,
+            )
+            for i in range(self.nrows * self.ncols)
+        ]
+
+
+    def get_tree_dims(self):
+        """
+        get height and width if not set by user
+        """
+        if self.ncols * self.nrows < 4:
+            minx = 250
+            miny = 250
+        else:
+            minx = 200
+            miny = 140
+
+        # wider than tall
+        if self.layout in ("d", "u"):
+            self.width = (
+                self.width if self.width
+                else min(750, minx * self.ncols)
+            )
+            self.height = (
+                self.height if self.height
+                else min(750, miny * self.nrows)
+            )
+
+        else:
+            self.height = (
+                self.height if self.height 
+                else min(750, minx * self.nrows)
+            )
+            self.width = (
+                self.width if self.width
+                else min(750, miny * self.ncols)
+            )
+
+
 
 class CanvasSetup:
     """
@@ -42,7 +112,7 @@ class CanvasSetup:
         self.get_canvas_and_axes()
 
         # expand the domain/extents for the text
-        self.fit_tip_labels()
+        # self.fit_tip_labels()
 
         # ticks for tree and scalebar
         self.add_axes_style()
@@ -178,39 +248,41 @@ class CanvasSetup:
 
 
 
-    def fit_tip_labels(self):
-        """
-        Modifies display range to ensure tip labels fit. This is a bit hackish
-        still. The problem is that the 'extents' range of the rendered text
-        is totally correct. So we add a little buffer here. Should add for 
-        user to be able to modify this if needed. If not using edge lengths
-        then need to use unit length for treeheight.
-        """
-        # bail on unrooted for now; TODO
-        if self.style.layout == "c":
-            return
+    # def fit_tip_labels(self):
+    #     """
+    #     DEPRECATED SINCE V2 since Mark now sets its own extents correctly.
 
-        # if names
-        if self.lname:
-            # get ratio of names to tree in plot
-            ratio = max(self.lname / 10, 0.15)
+    #     Modifies display range to ensure tip labels fit. This is a bit hackish
+    #     still. The problem is that the 'extents' range of the rendered text
+    #     is not totally correct. So we add a little buffer here. Should add for 
+    #     user to be able to modify this if needed. If not using edge lengths
+    #     then need to use unit length for treeheight.
+    #     """
+    #     # bail on unrooted for now; TODO
+    #     if self.style.layout == "c":
+    #         return
 
-            # have tree figure make up 85% of plot
-            if self.style.use_edge_lengths:
-                addon = self.tree.treenode.height
-            else:
-                addon = self.tree.treenode.get_farthest_leaf(True)[1] + 1
-            addon *= ratio
+    #     # if names
+    #     if self.lname:
+    #         # get ratio of names to tree in plot
+    #         ratio = max(self.lname / 10, 0.15)
 
-            # modify display for layout
-            if self.style.layout == "r":
-                self.axes.x.domain.max = (addon / 2.) + self.style.xbaseline
-            elif self.style.layout == "l":
-                self.axes.x.domain.min = (-addon / 2.) + self.style.xbaseline
-                # self.axes.x.domain.min -= self.style.xbaseline
-            elif self.style.layout == "d":
-                self.axes.y.domain.min = (-addon / 2.) + self.style.ybaseline
-            elif self.style.layout == "u":
-                self.axes.y.domain.max = (addon / 2.) + self.style.ybaseline
+    #         # have tree figure make up 85% of plot
+    #         if self.style.use_edge_lengths:
+    #             addon = self.tree.treenode.height
+    #         else:
+    #             addon = self.tree.treenode.get_farthest_leaf(True)[1] + 1
+    #         addon *= ratio
 
-            # print(addon, ratio, self.axes.x.domain.min, self.axes.x.domain.max)
+    #         # modify display for layout
+    #         if self.style.layout == "r":
+    #             self.axes.x.domain.max = (addon / 2.) + self.style.xbaseline
+    #         elif self.style.layout == "l":
+    #             self.axes.x.domain.min = (-addon / 2.) + self.style.xbaseline
+    #             # self.axes.x.domain.min -= self.style.xbaseline
+    #         elif self.style.layout == "d":
+    #             self.axes.y.domain.min = (-addon / 2.) + self.style.ybaseline
+    #         elif self.style.layout == "u":
+    #             self.axes.y.domain.max = (addon / 2.) + self.style.ybaseline
+
+    #         # print(addon, ratio, self.axes.x.domain.min, self.axes.x.domain.max)
