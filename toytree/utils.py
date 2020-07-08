f8c977bb6b66d4c37057e4488e4a8a5aa1683935
eaton-lab <de2356@columbia.edu>
2020-07-08 14:57:13 -0400

    parse network treatment of hybrid tip nodes

--- toytree/utils.py
+++ toytree/utils.py
@@ -145,24 +145,24 @@ def parse_network(net, disconnect=True):
         npre, npost = post.split(")", 1)
         newpre = npre.split(":")[0] + "-" + npre.split(":")[-1]
         net = pre + ")" + newpre + npost
-    net = net.replace("#", "").replace(":::", "-")
+    net = net.replace(":::", "-")
 
     # parse cleaned newick and set empty gamma on all nodes
     net = toytree.tree(net, tree_format=1)
-    # net = net.set_node_values("gamma", default=0)
-    # net = net.set_node_values("hybrid", default=0)
 
-    # store admix data (NOT USED CURRENTLY)
+    # store admix data
     admix = {}
 
-    # traverse tree to find hybrid nodes
+    # Traverse tree to find hybrid nodes. If a hybrid node is labeled as a 
+    # distinct branch in the tree then it is dropped from the tree and 
     for node in net.treenode.traverse():
 
-        # find internal nodes
-        if len(node.children) == 1:
+        # find hybrid nodes as internal nchild=1, or external with H in name
+        if (len(node.children) == 1) or node.name.startswith("#H"):
 
             # assign name and gamma to hybrid nodes
             aname, aprop = node.name.split("-")
+            aname = aname.lstrip("#")
             node.name = aname
 
             # assign hybrid to closest nodes up and down from edge
@@ -172,16 +172,35 @@ def parse_network(net, disconnect=True):
 
             # disconnect node by connecting children to parent
             if disconnect:
-                node.up.children.remove(node)
-                for child in node.children:
-                    child.up = node.up
-                    node.up.children.append(child)
 
-            # store admix data by descendants (NOT USED CURR)
-            # if node.up.is_root():
+                # if tip is a hybrid
+                if not node.children:
+                    # get sister node
+                    sister = [i for i in node.up.children if i != node][0]
+
+                    # connect sister to gparent
+                    sister.up = node.up.up
+                    node.up.up.children.remove(node.up)
+                    node.up.up.children.append(sister)
+
+                # if hybrid is internal
+                else:
+                    node.up.children.remove(node)
+                    for child in node.children:
+                        child.up = node.up
+                        node.up.children.append(child)
+
+            # store admix data by descendants but remove hybrid tips
             desc = node.get_leaf_names()
+            if aname in desc:
+                desc = [i for i in node.up.get_leaf_names() if i != aname]
+            desc = [i for i in desc if not i.startswith("#H")]
+
+            # put this node into admix
             if aname not in admix:
                 admix[aname] = (desc, aprop)
+
+            # matching edge in admix, no arrange into correct order by minor
             else:
                 # this is the minor edge
                 if aprop < admix[aname][1]:
@@ -192,6 +211,8 @@ def parse_network(net, disconnect=True):
                         {}, 
                         str(round(float(aprop), 3)),
                     )
+
+                # this is the major edge
                 else:
                     admix[aname] = (
                         desc, 
@@ -201,19 +222,9 @@ def parse_network(net, disconnect=True):
                         str(round(float(admix[aname][1]), 3)),
                     )
 
-            # # store admix data by descendants (NOT USED CURR)
-            # aedge = (node.up.name, node.children[0].name)
-            # if aname not in admix:
-            #     admix[aname] = (aedge, aprop)
-            # else:
-            #     # this is the minor edge
-            #     if aprop < admix[aname][1]:
-            #         admix[aname] = (aedge, admix[aname][0], aprop)
-            #     else:
-            #         admix[aname] = (admix[aname][0], aedge, admix[aname][1])                    
-
     # update coords needed if node disconnection is turned back on.
     net._coords.update()
+    net = net.ladderize()
     return net, admix
 
 
