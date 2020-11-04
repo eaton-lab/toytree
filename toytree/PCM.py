49c6cce4eebcef4710a9dc24f6ae35b2089769b9
Deren <de2356@columbia.edu>
2020-11-04 09:16:49 -0500

    added DR code to pcm

--- toytree/PCM.py
+++ toytree/PCM.py
@@ -4,7 +4,12 @@
 PCM: phylogenetic comparative methods tools
 """
 
+import os
+import time
 import numpy as np
+import pandas as pd
+import toytree
+
 
 
 class PCM:
@@ -52,6 +57,7 @@ class PCM:
 
 
 
+
 def VCV(tree):
     """
     Return the variance co-variance metrix representing the tree topology.
@@ -95,6 +101,7 @@ def PIC(tree, feature):
 
     # return dictionary mapping nodes to (mean, var, contrast, cvar)
     return results
+
 
 
 def dynamicPIC(node, data, results):
@@ -151,6 +158,164 @@ def dynamicPIC(node, data, results):
     # store in dictionary and 
     results[node] = (Xk, vk, Xi - Xj, vi + vj)
     return results
+
+
+
+def calculate_ES(tree):
+    "Return DataFrame with equal splits measure sensu Redding and Mooers 2006"
+    # dataframe for storing results
+    df = pd.DataFrame(columns=["DR"], index=tree.get_tip_labels())
+
+    # traverse up to root from each tip
+    for idx in range(tree.ntips):
+        node = tree.idx_dict[idx]
+        DR = 0
+        j = 1
+        while node.up:
+            DR += node.up.dist / (2 ** j)
+            node = node.up
+            j += 1
+        df.iloc[idx, 0] = DR
+    return df
+
+
+
+def calculate_DR(tree):
+    "Returns a dataframe with tip-level diversification rates sensu Jetz 2012"
+    # ensure tree is a tree
+    tree = toytree.tree(tree)
+    return 1 / calculate_ES(tree)
+
+
+
+def calculate_tip_level_diversification(trees, njobs=1):
+    """
+    Returns a dataframe with tip-level diversification rates calculated
+    across a set of trees. Enter a multitree object or, for very large trees,
+    you can enter a generator of newick strings (see example).
+
+    Parameters:
+    -----------
+    trees (Toytree, Multitree, or newick file):
+        One or more ultrametric trees in newick format. For ultra large
+        datasets use a file as input.
+
+    njobs (int):
+        Distribute N jobs in parallel usign ProcessPoolExecutor
+
+    Returns:
+    --------
+    df (pandas.DataFrame):
+        The raw DF based on branch lengths of the tree.
+
+    Examples:
+    ---------
+    # using one or more trees
+    df = calculate_tip_level_diversification(trees)
+
+    # loading super large trees
+    with open(hugetreefile, 'r') as treegenerator:
+        df = calculate_tip_level_diversification(treegenerator, njobs=20)
+    """
+
+    # load data and metadata from newick, toytree, or multitree
+    if isinstance(trees, str) and os.path.exists(trees):
+        with open(trees) as tree_generator:
+            tre = toytree.tree(next(tree_generator))
+            ntips = tre.ntips
+            ntrees = sum(1 for i in tree_generator) + 1
+            tiporder = tre.get_tip_labels()
+        itertree = open(trees, 'r')
+    elif isinstance(trees, toytree.Toytree.ToyTree):
+        itertree = iter([trees])
+        ntrees = len(trees)
+        ntips = trees.ntips
+        tiporder = trees.get_tip_labels()
+        topen = None
+    elif isinstance(trees, toytree.Multitree.MultiTree):
+        itertree = iter(i for i in trees)
+        ntrees = len(trees)
+        ntips = trees.treelist[0].ntips
+        tiporder = trees.treelist[0].get_tip_labels()
+        topen = None
+
+    # array to store results 
+    tarr = np.zeros((ntips, ntrees))
+
+    # run non-parallel calculations
+    if njobs == 1:
+        for tidx, tree in enumerate(itertree):
+            df = calculate_DR(tree)
+            tarr[:, tidx] = df.loc[tiporder, "DR"]
+
+    # or, distribute jobs in parallel (py3 only)
+    else:
+        from concurrent.futures import ProcessPoolExecutor 
+        pool = ProcessPoolExecutor(njobs)
+        treegen = iter(trees)
+        rasyncs = {}
+
+        # submit as many jobs as there are cores
+        for job in range(njobs):
+            tree = next(itertree)
+            rasyncs[job] = pool.submit(
+                calculate_DR,
+                (tree),
+            )
+
+        # as each job finished submit a new one until none are left.
+        # this allows avoiding loading all trees simultaneously.
+        tidx = 0
+        while 1:
+            # get finished jobs
+            finished = [i for i in rasyncs if rasyncs[i].done()]
+    
+            # store results and append new job to the engine
+            for job in finished:
+
+                # store result
+                result = rasyncs[job].result()
+                tarr[:, tidx] = result.loc[tiporder, "DR"]
+                tidx += 1
+                del rasyncs[job]
+
+                # append new job unless no jobs left
+                try:
+                    tree = next(itertree)
+                    rasyncs[job] = pool.submit(
+                        calculate_DR,
+                        (tree),
+                    )
+                except StopIteration:
+                    pass
+
+            # wait for next check
+            time.sleep(0.5)           
+
+            # wait until all jobs finish
+            if not rasyncs:
+                break
+        pool.shutdown()
+
+    # close the file handle, if exists
+    if hasattr(itertree, "close"):
+        itertree.close()
+
+    # calculate summary of DR across the distribution of trees.
+    arr = pd.DataFrame(tarr, index=tiporder)
+    df = pd.DataFrame({
+        'mean': arr.mean(1),
+        # 'harmMean': hmean(arr, axis=1),
+        'median': arr.median(1),
+        'std': arr.std(1),
+        '2.5%': np.percentile(arr, 0.025, axis=1),
+        '97.5%': np.percentile(arr, 0.975, axis=1),
+        'min': arr.min(1),
+        'max': arr.max(1),
+    })
+    return df
+
+
 
 
 # single test
