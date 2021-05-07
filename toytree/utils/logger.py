4e9239cd958265ad751ee60f06d18a8cb1e76c3a
Deren <de2356@columbia.edu>
2021-05-07 19:04:55 -0400

    logging module added

--- toytree/utils/logger.py
+++ toytree/utils/logger.py
@@ -8,24 +8,40 @@ import sys
 from loguru import logger
 
 
-def set_loglevel(loglevel="DEBUG"):#, logfile=None):
+LOGFORMAT = (
+    "{time:hh:mm} | {level: <7} | "
+    "<b><magenta>{function: <10}</magenta></b> | "
+    "<level>{message}</level>"
+)
+
+def colorize():
     """
-    Config and start the logger
+    colorize the logger if stderr is IPython/Jupyter or a terminal (TTY)
     """
-    config = {
-        "handlers": [
-            {
-                "sink": sys.stderr, 
-                "colorize": True,
-                "format": (
-                    "{time:hh:mm:ss.SSS} <level>{level: <7}</level> <white>|</white> "
-                    "<magenta>{file: <11}</magenta> <white>|</white> "
-                    "<black>{function: <16}</black> <white>|</white> "
-                    "<level>{message}</level>"
-                ),
-                "level": loglevel,
-                },
-        ]
-    }
+    try:
+        import IPython
+        tty1 = bool(IPython.get_ipython())
+    except ImportError:
+        tty1 = False
+    tty2 = sys.stderr.isatty()
+    if tty1 or tty2:
+        return True
+    return False
+
+def set_loglevel(loglevel="INFO"):
+    """
+    Set the loglevel for loguru logger. Using 'enable' here as 
+    described in the loguru docs for logging inside of a library.
+    This sets the level at which logger calls will be displayed 
+    throughout the rest of the code.
+    """
+    config = {}
+    config["handlers"] = [{
+        "sink": sys.stderr,
+        "format": LOGFORMAT,
+        "level": loglevel,
+        "colorize": colorize(),
+        # "enqueue": True,
+    }]
     logger.configure(**config)
     logger.enable("toytree")
