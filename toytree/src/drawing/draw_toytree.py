bc86c39474ce04245fbe7409d66e66688840f9ab
Deren <de2356@columbia.edu>
2021-05-30 14:14:28 -0400

    removed old code, and logger

--- toytree/src/drawing/draw_toytree.py
+++ toytree/src/drawing/draw_toytree.py
@@ -5,8 +5,7 @@ Function to parse user args to toytree.draw() and return drawing.
 See full args/docstring at toytree.core.Toytree.ToyTree.draw()
 """
 
-from loguru import logger
-
+import sys
 from toytree.src.drawing.tree_style import TreeStyle
 from toytree.src.drawing.style_checker import StyleChecker
 from toytree.src.drawing.render import ToytreeMark
@@ -30,7 +29,7 @@ def draw_toytree(**kwargs):
 
     # warn user if they entered kwargs that arent't supported.
     if kwargs.get("kwargs"):
-        logger.warning(
+        sys.stderr.write(
             f"Unrecognized arguments skipped: {list(kwargs['kwargs'])}."
             "\nCheck the docs, argument names may have changed.")
         kwargs.pop('kwargs')
@@ -59,7 +58,7 @@ def draw_toytree(**kwargs):
 
     # debugging returns the mark and prints the modified kwargs
     if kwargs.get('debug'):
-        logger.debug(user)
+        print(user)
         return fstyle
 
     # get canvas and axes
@@ -73,135 +72,3 @@ def draw_toytree(**kwargs):
     # add mark to axes
     axes.add_mark(mark)
     return canvas, axes, mark
-
-
-# DEPREXATED OLD FUNC
-# def draw_toytree2(
-#     self,
-#     tree_style:Optional[str]=None,
-#     height:Optional[int]=None,
-#     width:Optional[int]=None,
-#     axes:Optional[toyplot.coordinates.Cartesian]=None,    
-#     layout:Optional[str]=None,
-#     tip_labels:Union[bool,Iterable]=None,
-#     tip_labels_colors=None,
-#     tip_labels_style=None,
-#     tip_labels_align=None,
-#     node_labels=None,
-#     node_labels_style=None,
-#     node_sizes=None,
-#     node_colors=None,
-#     node_style=None,
-#     node_hover=None,
-#     node_markers=None,
-#     edge_colors=None,
-#     edge_widths=None,
-#     edge_type=None,
-#     edge_style=None,
-#     edge_align_style=None,
-#     use_edge_lengths=None,
-#     scalebar=None,
-#     padding=None,
-#     xbaseline=None,
-#     ybaseline=None,
-#     admixture_edges=None,
-#     shrink=None,
-#     fixed_order=None,
-#     fixed_position=None,
-#     **kwargs,
-#     ) -> Tuple:
-#     """
-#     Draw toytree (see toytree.core.Toytree.draw) for full docstring.
-#     """
-#     # update kwargs to merge it with user-entered arguments:
-#     userargs = {
-#         "height": height,
-#         "width": width,
-#         "layout": layout,
-#         "tip_labels": tip_labels,
-#         "tip_labels_colors": tip_labels_colors,
-#         "tip_labels_align": tip_labels_align,
-#         "tip_labels_style": tip_labels_style,
-#         "node_labels": node_labels,
-#         "node_labels_style": node_labels_style,
-#         "node_sizes": node_sizes,
-#         "node_colors": node_colors,
-#         "node_hover": node_hover,
-#         "node_style": node_style,
-#         "node_markers": node_markers,
-#         "edge_type": edge_type,
-#         "edge_colors": edge_colors,
-#         "edge_widths": edge_widths,
-#         "edge_style": edge_style,
-#         "edge_align_style": edge_align_style,
-#         "use_edge_lengths": use_edge_lengths,
-#         "scalebar": scalebar,
-#         "padding": padding,
-#         "xbaseline": xbaseline, 
-#         "ybaseline": ybaseline,
-#         "admixture_edges": admixture_edges,
-#         "shrink": shrink,
-#         "fixed_order": fixed_order,
-#         "fixed_position": fixed_position
-#     }
-
-#     # shortcut name for tree style
-#     if kwargs.get("ts"):
-#         tree_style = kwargs.get("ts")
-
-#     # use a base style preset over which other options override
-#     if tree_style:
-#         curstyle = TreeStyle(tree_style[0])
-
-#     # or use current tree settings (DEFAULT unless changed by user)
-#     else:           
-#         curstyle = self.style.copy()
-
-#     # optionally override current style with style args entered to draw()
-#     kwargs.update(userargs)
-#     user = dict(
-#         ("_" + i, j) if isinstance(j, dict) else (i, j)
-#         for (i, j) in kwargs.items() if j is not None
-#     )
-#     curstyle.update(user)
-
-#     # warn user if they entered kwargs that arent't supported.
-#     allkeys = list(userargs.keys()) + ["debug", "ts"]
-#     unrecognized = [i for i in kwargs if i not in allkeys]
-#     if unrecognized:
-#         logger.warning("Unrecognized arguments skipped: {}"
-#             "\ncheck the docs, argument names may have changed."
-#             .format(unrecognized))
-
-#     # update coords based on layout
-#     edges = self._coords.get_edges()
-#     if layout == 'c':
-#         verts = self._coords.get_radial_coords(curstyle.use_edge_lengths)
-#     else:
-#         verts = self._coords.get_linear_coords(
-#             curstyle.layout, 
-#             curstyle.use_edge_lengths,
-#             fixed_order,
-#             fixed_position,
-#         )
-#     logger.debug('verts\n{}'.format(verts))
-
-#     # check all styles
-#     fstyle = StyleChecker(self, curstyle).style
-
-#     # debugging returns the mark and prints the modified kwargs
-#     if kwargs.get('debug'):
-#         logger.debug(user)
-#         return fstyle
-
-#     # get canvas and axes
-#     csetup = CanvasSetup(self, axes, fstyle)
-#     canvas = csetup.canvas
-#     axes = csetup.axes
-
-#     # generate toyplot Mark
-#     mark = ToytreeMark(ntable=verts, etable=edges, **fstyle.to_dict())
-
-#     # add mark to axes
-#     axes.add_mark(mark)
-#     return canvas, axes, mark
