671079bec612c720a408d53f1160c7f28bdc5f4a
Deren <de2356@columbia.edu>
2021-05-30 11:19:22 -0400

    simplify

--- toytree/utils/exceptions.py
+++ toytree/utils/exceptions.py
@@ -10,22 +10,16 @@ class ToytreeError(Exception):
         Exception.__init__(self, *args, **kwargs)
 
 
-class TreeError(Exception):
-    "A problem occurred during a TreeNode operation"
-    def __init__(self, value=''):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
+class TreeNodeError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
 
 
 class NewickError(Exception):
-    """Exception class designed for NewickIO errors."""
     def __init__(self, value):
         Exception.__init__(self, value)
 
 
 class NexusError(Exception):
-    """Exception class designed for NewickIO errors."""
     def __init__(self, value):
         Exception.__init__(self, value)
