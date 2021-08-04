3dc05d20c41a7626f027eab73e7cfb3f917b7f54
Deren <de2356@columbia.edu>
2021-08-04 12:32:24 -0400

    new style submodule and custom classes

--- toytree/core/style/color.py
+++ toytree/core/style/color.py
@@ -1,50 +1,174 @@
 #!/usr/bin/env python
 
-
 """
-Attempt to simplify toyplot.color representation and make it 
-serializable with pydantic models.
-
-dtype = {
-    "names": ["r", "g", "b", "a"], 
-    "formats": ["float64", "float64", "float64", "float64"],
-    }
+The ToyColor class is used _internally_ by toytree to store 
+colors while plotting. This object is a superclass of a ndarray,
+consistent with the color representation in toyplot. It differs
+in that it can be serialized to JSON by pydantic by interchangeably
+converting to/from a Color object, which is made convenient by 
+the ColorMixer superclass.
 """
 
 # pylint: disable=no-member
 
-import xml
+from typing import Union, Tuple
+import xml.etree.ElementTree as xml
 import numpy as np
 import toyplot
-from pydantic import BaseModel
-from pydantic.color import Color
+import toyplot.color
+import pydantic
+import pydantic.color
+from toytree.core.drawing.render import style_to_string
+from toytree.utils.exceptions import ToytreeError
+
+
+DTYPE = {
+    "names": ["r", "g", "b", "a"], 
+    "formats": ["float64", "float64", "float64", "float64"],
+}
 
 
 class ToyColor(np.ndarray):
     """
-
+    ToyColor is a superclass of numpy.ndarray, and 
     """
-    def __init__(self, r:float, g:float, b:float, a:float=1.0):
-        super().__init__([r, g, b, a], dtype=np.float64)
+    def __new__(cls, color):
+        color = Color(color)
+        obj = np.array(color.rgba, dtype=DTYPE).view(cls)
+        return obj   
+    
+    @property
+    def css(self):
+        """Returns a name or hex value of a color"""
+        return Color(self).css
+
+    @property
+    def rgba(self):
+        """Returns a tuple of (r,g,b,a) as floats."""
+        return Color(self).rgba
+    
+    @property
+    def color(self):
+        """Returns a ..."""
+        return Color(self)
+
+
+
+class Color(pydantic.color.Color):
+    """
+    Flexible color parser class to get css or rgba tuple 
+    representation, and easier to work with than toyplot.color 
+    arrays. Serializable with pydantic, so this is the object type
+    that is stored in TreeStyle class objects.
+
+    - rgba = (r:float, g:float, b:float, a:float)
+    - rgb = (r:float, g:float, b:float, a:float)
+    - hex = "#000000"
+    - css = "cornflower"
+    - color = pydantic.color.Color
+
+    Attributes
+    ----------
+    css: str
+    rgba: Tuple[float, float, float, float]
+    """
+    def __init__(
+        self,
+        color: Union[str, np.ndarray, pydantic.color.Color],
+        ):
+
+        # attrs to fill
+        self.css: str = None
+        self.rgba: Tuple[float, float, float, float] = None
+        self.array: np.ndarray = None
+
+        # input is a css string (parse input based on type
+        if isinstance(color, str):
+            self.css = color
+            if not self.css:
+                raise ToytreeError(f"color str {color} not recognized")
+            self.array = toyplot.color.css(color)
+            self.rgba = tuple(float(self.array[i]) for i in 'rgba')
+            rgb_ints = [int(i * 255) for i in self.rgba[:3]]
+            rgba = rgb_ints + [self.rgba[3]]
+            super().__init__(rgba)
+
+        # input is an ndarray (e.g., toyplot.color ndarray, 1-d 4 floats)
+        elif isinstance(color, np.ndarray):
+            assert all(color[i] <= 1 for i in 'rgba'), (
+                "color array should contain floats in range [0, 1]")
+            self.array = color
+            self.css = toyplot.color.to_css(color)
+            if not self.css:
+                raise ToytreeError(f"color array {color} not recognized")
+            self.rgba = tuple(float(self.array[i]) for i in 'rgba')                
+            rgb_ints = [int(i * 255) for i in self.rgba[:3]]
+            rgba = rgb_ints + [self.rgba[3]]
+            super().__init__(rgba)
+
+        # input is a pydantic Color object
+        elif isinstance(color, pydantic.color.Color):
+            super().__init__(color)
+            self.css = color.as_named()
+            rgba = self.as_rgb_tuple(alpha=True)
+            rgb_floats = [i / 255. for i in rgba[:3]]
+            self.rgba = rgb_floats + [float(rgba[3])]
+            self.array = toyplot.color.rgba(*self.rgba)
+        else:
+            raise ToytreeError(f"color arg {color} not supported")
+
+        # store style dict
+        self.style = {
+            "float": "left",
+            "width": "20px",
+            "height": "20px",
+            "margin-right": "5px",
+            "background-color": self.css,
+        }
+
 
     def _repr_html_(self):
         """
         Show color as a div in jupyter notebooks
         """
+        # create a root dom element
         root_xml = xml.Element(
             "div",
             style="overflow:hidden; height:auto",
-            attrib={"class": css_class})
-        for color in colors:
-            xml.SubElement(
-                root_xml,
-                "div",
-                style="float:left;width:20px;height:20px;margin-right:%spx;background-color:%s" % (margin, to_css(color)))
-        return xml.tostring(root_xml, encoding="unicode", method="html")
+            attrib={"class": "toytree-ToyColor"},
+        )
+        _ = xml.SubElement(
+            root_xml, "div",
+            style=style_to_string(self.style),
+        )
+        _ = xml.SubElement(
+            root_xml, "text",
+        ).text = (
+            "ColorMixer({:.2f}, {:.2f}, {:.2f}, {:.2f})"
+            .format(*self.rgba)
+        )
 
+        # convert deom to a simple html script
+        html = xml.tostring(root_xml, encoding="unicode", method="html")
+        return html
 
 
 
 if __name__ == "__main__":
 
+    import toytree
+
     PALETTE = toyplot.color.Palette()
+    COLOR = PALETTE[0]
+
+    col0 = Color(COLOR)
+    col1 = Color('red')
+    col2 = Color("#262626")
+
+    col0 = ToyColor(COLOR)
+    col1 = ToyColor("red")
+    col2 = ToyColor("#262626")
+    col3 = ToyColor(toytree.colors[0])
+
+    for col in (col0, col1, col2, col3):
+        print(f"{col.css}\t{col.rgba}\t{col}")
--- toytree/core/style/tree_style.py
+++ toytree/core/style/tree_style.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
 
 """
-NOT YET IMPLEMENTED
-
-Tree Styles with type checking and serialization.
-
-1. Attribute of ToyTree (downside is it slows initialization...)
-
+Tree Styles checked and expanded at .draw() or repr().
 
 Usage
 -----
-
 > toytree.tree(...)                   # DefaultTreeStyle() init
 > tree.draw(**kwargs)                 # dict or user args
 > style = DefaultTreeStyle(**kwargs)  # DefaultTreeStyle(**dict, **tree.style)
-
-# style is then expanded in tree-context to List len args
-
-
 """
 
-# pylint: disable=no-name-in-module, no-self-argument, no-self-use
-
+from typing import List, Tuple, Optional, Union, Iterable
+import json
 from enum import Enum
-from typing import List, Tuple, Optional, Union
+from dataclasses import dataclass, asdict, field
 import toyplot
-import numpy as np
-from pydantic import BaseModel, Field, validator
-from pydantic.color import Color
+from pandas import Series, DataFrame
+from numpy import ndarray
+from toytree.core.style.color import ToyColor, Color
+from toytree.utils.exceptions import ToytreeError
 
 
 COLORS1 = list(
@@ -37,146 +28,391 @@ COLORS2 = list(
 BLACK = toyplot.color.black
 
 
-
-class TreeStyles(str, Enum):
-    normal = 'n'
-    dark = 'd'
-    umlaut = 'o'
-    coalescent = 'c'
-
-class EdgeTypes(str, Enum):
+class EdgeType(str, Enum):
     cladogram = 'c'
     phylogram = 'p'
     bezier = 'b'
 
-class LayoutTypes(str, Enum):
+class LayoutType(str, Enum):
     right = 'right'
     left = 'left'
     down = 'down'
     up = 'up'
     circle = 'circular'
-    unrooted = 'unrooted'
+    #unrooted = 'unrooted'
+
+class SubStyle:
+    pass
+
+@dataclass
+class EdgeStyle(SubStyle):
+    stroke: Color = 'rgba(16.1%,15.3%,14.1%,1.000)'
+    stroke_width: float = 2.
+    stroke_linecap: str = "round"  # pydantic alias="stroke-linecap"
+    stroke_opacity: float = 1.
+    # stroke_dasharray: str = "2,4"
+
+@dataclass
+class NodeStyle:
+    fill: Color = 'rgba(40.0%,76.1%,64.7%,1.000)'
+    stroke: Optional[Color] = None
+    stroke_width: float = 1.0
+
+@dataclass
+class NodeLabelStyle:
+    fill: Color = 'rgba(16.1%,15.3%,14.1%,1.000)'
+    font_size: Union[int, str] = "9px"
+
+@dataclass
+class EdgeAlignStyle:
+    stroke: Color = 'rgba(66.3%,66.3%,66.3%,1.000)'
+    stroke_width: int = 2
+    stroke_linecap: str = "round"
+    stroke_dasharray: str = "2,4"
+
+@dataclass
+class TipLabelsStyle:
+    fill: Color = 'rgba(16.1%,15.3%,14.1%,1.000)'
+    fill_opacity: Union[str, float] = 1.0  # fill-opacity
+    font_size: Union[str, float] = 11  # "11px" 'font-size'
+    _toyplot_anchor_shift: Union[str, float] = 15  # '15px', -toyplot-anchor-shift
+    text_anchor: str = "start"  # text-anchor
 
 
-class Array:
-    """
-    Arrays are always converted to lists... still figuring out how to
-    best allow np.arrays into pydantic model...
-    """
+######################################################################
+#
+#  Base TreeStyle Class with validation functions
+#
+######################################################################
 
-class DefaultEdgeStyle(BaseModel):
-    stroke: Optional[Color] = Field("#262626")
-    stroke_width: float = Field(2, min=0, description="see edge_widths") 
-    stroke_linecap: str = Field("round", alias="stroke-linecap")
-    stroke_opacity: float = Field(1, min=0.0, max=1.0)
-
-class DefaultNodeStyle(BaseModel):
-    fill: Optional[Color] = Field(COLORS1[0])
-    stroke: Optional[Color] = Field(None)
-    stroke_width: float = Field(1, min=0)
-
-class DefaultTreeStyle(BaseModel):
+@dataclass
+class TreeStyle:
+    # inherited from tree at validation steps
+    tree_style: Optional[str] = None
     height: Optional[int] = None
     width: Optional[int] = None
-    edge_type: EdgeTypes = 'p'
-    layout: LayoutTypes = 'r'
+    layout: LayoutType = 'r'
 
-    node_labels: Union[bool,List[str]] = Field(..., help="...")
-    node_colors: List[Tuple[float,float,float,float]] = Field(..., help="")
+    edge_type: EdgeType = 'p'
+    edge_colors: Union[Color, Iterable[Color], None] = None  # override by fill
+    edge_widths: Union[int, Iterable[int], None] = None  # override by stroke-width
+    edge_style: EdgeStyle = field(default_factory=EdgeStyle)
+    edge_align_style: EdgeAlignStyle = field(default_factory=EdgeAlignStyle)
 
-    edge_colors: Optional[List[Color]] = None
-    edge_widths: Optional[List[float]] = None
-    node_sizes: Optional[Union[int, List[int]]] = None
-    node_colors: Optional[Union[str,Color,List[str],List[Color]]] = COLORS1[0]
-    node_markers: List[str]=Field(None)
-    node_hover: bool=Field(False)
-    use_edge_lengths: bool=Field(False)
-    tip_labels: bool=Field(True)
-    tip_labels_colors: Optional[List[Color]] = None
+    node_mask: Union[bool, Iterable[bool], None] = None  # None = auto
+    node_labels: Union[bool, str, Iterable[str]] = False
+    node_colors: Union[Color, Iterable[Color], None] = None  # override by fill
+    node_sizes: Union[int, Iterable[int]] = 0
+    node_markers: Union[str, Iterable[str]] = "o"
+    node_hover: Union[bool, str, Iterable[str]] = None  # None = auto
+    node_style: NodeStyle = field(default_factory=NodeStyle)
+    node_labels_style: NodeLabelStyle = field(default_factory=NodeLabelStyle)
+
+    tip_labels: Union[bool, Iterable[str]] = True
+    tip_labels_colors: Union[str, Iterable[str], None] = None
     tip_labels_align: bool = False
-    scale_bar: bool = False
-    padding: int = Field(20, help="applied to Cartesian axes")
+    tip_labels_style: TipLabelsStyle = field(default_factory=TipLabelsStyle)
+    tip_labels_angles: Union[float, Iterable[float], None] = None
+
+    use_edge_lengths: bool = True
+    scalebar: bool = False
+    padding: int = 15
     xbaseline: int = 0
     ybaseline: int = 0
-    admixture_edges: List[Tuple[int, int]] = Field(None, help="(src, dest, prop, ...)")
+    admixture_edges: Union[List[Tuple[int, int]], None] = None
     shrink: int = 0
-    fixed_order: Optional[List[str]] = None
 
-    node_style: DefaultNodeStyle=Field(default_factory=DefaultNodeStyle)
-    edge_style: DefaultEdgeStyle=Field(default_factory=DefaultEdgeStyle)
 
-    class Config:
-        validate_assignment = True
-
-    @validator('node_colors')
-    def check_node_colors(cls, value) -> List[str]:
+    def validate(self, tree: 'toytree.ToyTree'):
         """
-        Converts input type to a tuple of rgba floats
-          str       | 'teal'                    | [(0.4, 0.7, 0.5, 1.0), ...
-          ndarray   | (0.4, 0.7, 0.6, 1.0)      | [(0.4, 0.7, 0.5, 1.0), ...
-          List[str] | ['teal', 'orange', ...]   | [(0.4, 0.7, 0.5, 1.0), ...
-          List[ndarray] | ['teal', 'orange', ...]   | [(0.4, 0.7, 0.5, 1.0), ...          
+        Expand iterable style entires to None or ndarray, and
+        convert color args to CSS strings. This function is called
+        before passing style args to ToyTreeMark.
         """
-        # it may be a color 
-        if isinstance(value, np.ndarray):  # it is a color array
-            value = list(value)
-        if isinstance(value, np.ndarray):  # it is an array of color str
-            value = list(value)
-        if isinstance(value, np.ndarray):  # it is an array of color arrays
-            value = list(value)
-        if isinstance(value, np.ndarray):  # it is a str
-            value = list(value)
-        if isinstance(value, np.ndarray):  # it is an Iterable of str
-            value = list(value)
+        self.tree = tree  # ignore
+        self._validate_node_colors()
+        self._validate_node_mask()
+        self._validate_node_sizes()
+        self._validate_node_labels()
+        self._validate_node_style()
+        self._validate_tip_labels()
+        self._validate_tip_labels_colors()
+
+    def _serialize_arrays_and_colors(self):
+        """
+        Convert all ndarrays to lists, and colors to strings, for
+        serialization of JSON. This does not perform full expansion
+        and validation of all style args. This is called in the
+        style repr() for writing to JSON, but actually used for
+        validating style args for drawings (see .validate())
+        """
+        self._validate_node_colors()
+        self._validate_tip_labels_colors()
+        self._serialize_arrays()
+        # ... substyledicts
+
+    def _serialize_arrays(self):
+        """
+        Convert any ndarray inputs to lists for more reliable type
+        checking without having to array dtypes, etc.
+        TODO: could check shape and type here while they are arrays,
+        but problem is we don't know for sure they will be arrays...
+        so should we cast everything to an array, and then
+        optionally back to list again for serial? Check the Mark
+        to see if it actually benefits from arrays?
+        """
+        arrayed = [
+            "node_mask",
+            "node_sizes",
+            "node_labels",
+            "tip_labels",
+        ]
+        for key in arrayed:
+            values = getattr(self, key)
+            if isinstance(values, ndarray):
+                setattr(self, key, values.tolist())
+            elif isinstance(values, DataFrame):
+                setattr(self, key, values.iloc[:, 0].tolist())
+            elif isinstance(values, Series):
+                setattr(self, key, values.tolist())
+
+    def _validate_node_colors(self):
+        """
+        Sets node_colors to ndarray[str] or None, and sets
+        node_style.fill to None or single color value.
+        """
+        # parse as a single color
+        if self.node_colors is None:
+            return
+        try:
+            css_color = ToyColor(self.node_colors).css
+            self.node_colors = None
+            self.node_style.fill = css_color
+        except ToytreeError:
+            # TODO: could expand a color map here?
+            self.node_style.fill = None
+            self.node_colors = toyplot.broadcast.pyobject(
+                [ToyColor(i).css for i in self.node_colors],
+                self.tree.nnodes,
+            ).astype(str)
+
+    def _validate_node_mask(self):
+        """
+        Sets node_mask to ndarray[bool].
+            None: special to hide tips only
+            True: show all
+            False: hide all
+            Iterable: custom
+        """
+        if self.node_mask in [True, False]:
+            self.node_mask = [self.node_mask] * self.tree.nnodes
+        if self.node_mask is None:
+            self.node_mask = [True] * self.tree.ntips
+            self.node_mask += [False] * (self.tree.nnodes - self.tree.ntips)
+        self.node_mask = toyplot.broadcast.pyobject(
+            self.node_mask, self.tree.nnodes).astype(bool)
+
+    def _validate_node_sizes(self):
+        """
+        Sets node_sizes to ndarray[float]
+        """
+        self.node_sizes = toyplot.broadcast.scalar(
+            self.node_sizes, self.tree.nnodes)
+
+    def _validate_node_labels(self):
+        """
+        Sets node_labels to ndarray[str] or None
+        """
+        if self.node_labels is True:
+            self.node_labels = range(self.tree.nnodes)
+        elif self.node_labels is False:
+            self.node_labels = None
+        elif self.node_labels in self.tree.features:
+            self.node_labels = self.tree.get_node_data(self.node_labels).tolist()
+        elif isinstance(self.node_labels, str):
+            self.node_labels = [self.node_labels] * self.tree.nnodes
         else:
-            raise TypeError(f"node_labels type is invalid: {value}")
-        return value
+            pass
+        if self.node_labels is not None:
+            self.node_labels = toyplot.broadcast.pyobject(
+                self.node_labels, self.tree.nnodes).astype(str)
 
-    @validator('node_labels')
-    def check_node_labels(cls, value, values) -> List[str]:
+    def _validate_node_style(self):
         """
-        Converts input type to a list of strings. 
-          bool -> [' ', ' ', ' ', ...] or ['', '', '', ...]
-          List[str] -> ['a', 'b', 'c']
+        Sets node_style.fill and .stroke to str
         """
-        if isinstance(value, list):
-            return value
-        if isinstance(value, np.ndarray):
-            return list(value)
-        if value is True:
-            return [" "] * values['_nnodes']
-        if value is False:
-            return [""] * values['_nnodes']
-        raise TypeError(f"node_labels type is invalid: {value}")
+        if self.node_style.stroke is not None:
+            self.node_style.stoke = ToyColor(self.node_style.stroke).css
+        if self.node_style.fill is not None:
+            self.node_style.fill = ToyColor(self.node_style.fill).css
+
+    def _validate_tip_labels_colors(self):
+        """
+        Sets tip_labels_colors to ndarray[str] or None, and sets
+        tip_labels.style.fill to None or single color.
+        """
+        if self.tip_labels_colors is None:
+            return
+        try:
+            css_color = ToyColor(self.tip_labels_colors).css
+            self.tip_labels_colors = None
+            self.tip_labels_style.fill = css_color
+        except ToytreeError:
+            self.tip_labels_style.fill = None
+            self.tip_labels_colors = toyplot.broadcast.pyobject(
+                [ToyColor(i).css for i in self.tip_labels_colors],
+                self.tree.ntips,
+            ).astype(str)
+
+    def _validate_tip_labels(self):
+        """
+        Expand tip labels to a list of strings of len=ntips.
+        """
+        if self.tip_labels is True:
+            self.tip_labels = self.tree.get_tip_labels()
+        elif self.tip_labels is False:
+            self.tip_labels = [None] * self.tree.ntips
+        else:
+            pass
+        self.tip_labels = toyplot.broadcast.pyobject(
+            self.tip_labels, self.tree.ntips).astype(str)
+
+    def _validate_tip_labels_angles(self):
+        """
+        sets tip_labels_angles to ndarray[float]. None is auto layout.
+        """
+        if self.tip_labels_angles is None:
+            if self.layout == 'c':
+                tip_radians = np.linspace(0, -np.pi * 2, self.tree.ntips + 1)[:-1]
+                angles = np.array([np.rad2deg(abs(i)) for i in tip_radians]) * -1
+            elif self.layout in ["u", "d"]:
+                angles = -90
+            else:
+                angles = 0
+        else:
+            angles = self.tip_labels_angles
+        self.tip_labels_angles = (
+            toyplot.broadcast.scalar(angles, self.tree.ntips))
 
 
-    # @classmethod
-    # def validate(cls, val):
-    #     if isinstance(val, np.ndarray):
-    #         try:
-    #             val = map(toyplot.color.to_css, val)
-    #         except Exception:
-    #             print("EXCEPTION", val)
-    #         return list(val)
-    #     return val
 
 
+
+    def dict(self, serialize=False):
+        """Returns a dictionary of the TreeStyle"""
+        if serialize:
+            self._serialize_arrays()
+        return asdict(self)
+
+    def json(self):
+        """Returns a JSON serialized dict of the TreeStyle"""
+        return json.dumps(self.dict(serialize=True), indent=4)
+
+    def __repr__(self):
+        """Recursive dict to JSON representation"""
+        return self.json()
+
+
+
+######################################################################
+#
+# BUILTIN TREE STYLES
+#
+######################################################################
+
+
+@dataclass(repr=False)
+class NormalTreeStyle(TreeStyle):
+    tree_style: str = 'n'
+    node_sizes: int = 15
+
+@dataclass(repr=False)
+class SimpleTreeStyle(TreeStyle):
+    tree_style: str = 's'
+    layout: LayoutType = "r"
+    node_labels: bool = True
+    node_sizes: int = 15
+    node_style: NodeStyle = field(
+        default_factory=lambda: NodeStyle(
+            stroke="#262626",
+            stroke_width=1.5,
+            fill="lightgrey",
+        )
+    )
+    tip_labels: bool = True
+    use_edge_lengths: bool = False
+
+@dataclass(repr=False)
+class PhyloTreeStyle(TreeStyle):
+    tree_style: str = 'p'
+    layout: LayoutType = 'd'
+    edge_type: EdgeType = 'c'
+    edge_widths: Union[int, Iterable[int], str] = "Ne"  # special
+    node_labels: bool = True
+    node_sizes: int = 15
+    node_hover: bool = False
+    node_style: NodeStyle = field(
+        default_factory=lambda: NodeStyle(
+            stroke="#262626",
+            stroke_width=1.0,
+        )
+    )
+    tip_labels: bool = True
+    tip_labels_align: bool = False
+    scalebar: bool = True
+    use_edge_lengths: bool = True
+
+@dataclass(repr=False)
+class UmlautTreeStyle(TreeStyle):
+    tree_style: str = "o"
+    edge_type: EdgeType = 'c'
+    layout: LayoutType = 'r'
+    node_sizes: int = 8
+    tip_labels: bool = True
+    tip_labels_align: bool = True
+    edge_style: EdgeStyle = field(
+        default_factory=lambda: EdgeStyle(
+            stroke='#262626',
+            stroke_width=2,
+        )
+    )
+    node_style: NodeStyle = field(
+        default_factory=lambda: NodeStyle(
+            stroke="white",
+            stroke_width=1.5,
+            fill='rgba(10.6%,62.0%,46.7%,1.000)',
+        )
+    )
+
+def get_tree_style(ts: str="n") -> TreeStyle:
+    """
+    Convenience function to return an initialized TreeStyle
+    of a given builtin style.
+    """
+    ts = ts.lower()[0]
+    style_dict = {
+        "n": NormalTreeStyle(),
+        "p": PhyloTreeStyle(),
+        "o": UmlautTreeStyle(),
+    }
+    return style_dict[ts]
 
 
 if __name__ == "__main__":
 
-    # print(DefaultEdgeStyle(stroke='red').json(indent=4))
-    # print(DefaultNodeStyle(stroke=None).json(indent=4))
     import toytree
+    import numpy as np
+
     TREE = toytree.rtree.unittree(10)
-    print(COLORS1)
-
-    tstyle = DefaultTreeStyle(
-        ntips=TREE.ntips,
-        nnodes=TREE.nnodes,
-        node_labels=False,#TREE.get_node_labels("idx"),
-        #node_sizes=list(np.arange(10)),
-        #node_colors=[i for i in toyplot.color.Palette()],
+    tstyle = NormalTreeStyle(
+        node_labels="hi", #np.zeros(10),
+        node_hover="yo",
+        height=400,
+        width=400,
+        edge_type="c",
+        node_sizes=np.zeros(5),
+        node_colors=list(toyplot.color.Palette()),
+        edge_colors='red',
     )
-
-    print(tstyle.json(indent=4))
+    tstyle = UmlautTreeStyle(node_colors='red')
+    print(tstyle)
