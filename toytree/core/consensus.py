6877a6f33509ca64c989e6529060a931683f21ae
Deren <de2356@columbia.edu>
2021-08-12 12:19:36 -0400

    faster consensus inference, and not requiring ladderizing

--- toytree/core/consensus.py
+++ toytree/core/consensus.py
@@ -3,21 +3,18 @@
 """
 Get the consensus tree from a list of trees.
 
-TODO: 
+TODO:
     - maybe faster using Python array.
     - calculate mean branch lengths on consensus tree.
     - calculate mean heights keeping 0 min if ultrametric.
+    - unroot and then re-root at end?
 """
 
+from typing import List
 from hashlib import md5
 from collections import defaultdict
 import numpy as np
-
-from toytree.core.tree import ToyTree
-from toytree.core.treenode import TreeNode
-# from toytree.utils.exceptions import ToytreeError
-# from toytree.utils.utils import bpp2newick
-
+import toytree
 
 
 class ConsensusTree:
@@ -25,11 +22,16 @@ class ConsensusTree:
     An extended majority rule consensus function.
     Modelled on the similar function from scikit-bio tree module. If
     cutoff=0.5 then it is a normal majority rule consensus, while if
-    cutoff=0.0 then subsequent non-conflicting clades are added to the tree.
+    cutoff=0.0 then subsequent non-conflicting clades are added to
+    the tree.
     """
-    def __init__(self, treelist, best_tree=None, cutoff=0.0):
+    def __init__(
+        self,
+        treelist: List['toytree.ToyTree'],
+        best_tree: 'toytree.ToyTree'=None,
+        cutoff: float=0.0,
+        ):
 
-        # parse args
         self.treelist = treelist
         self.best_tree = best_tree
         if self.best_tree is not None:
@@ -42,10 +44,13 @@ class ConsensusTree:
         # attrs to fill
         self.namedict = None
         self.treedict = {}
+        self.idx_to_tree = {}
+        self.idx_to_count = {}
+        self.hash_to_idx = {}
         self.clade_counts = None
         self.fclade_counts = None
 
-        # results 
+        # results
         self.ttree = None
         self.nodelist = None
 
@@ -53,16 +58,18 @@ class ConsensusTree:
 
 
     def update(self):
-
+        """
+        ...
+        """
         # hash a dict to remove duplicate trees
-        self.hash_trees()
+        self.new_hash_trees()
 
         # map onto best_tree of infer majrule consensus
         if self.best_tree is not None:
             self.map_onto_best_tree()
 
         else:
-            # Find which clades occured with freq > cutoff. 
+            # Find which clades occured with freq > cutoff.
             # Fills namedict, clade_counts
             self.find_clades()
 
@@ -75,9 +82,25 @@ class ConsensusTree:
             self.build_trees()  # fclade_counts, namedict)
             ## todo. make sure no singleton nodes were left behind ...
 
+    def new_hash_trees(self):
+        """
+        Get the counts of all unique unrooted trees.
+        """
+        for idx, tree in enumerate(self.treelist):
+            utree = tree.unroot()
+            hashed = utree.treenode.get_topology_id()
+            if hashed not in self.hash_to_idx:
+                self.hash_to_idx[hashed] = idx
+                self.idx_to_tree[idx] = utree
+                self.idx_to_count[idx] = 1
+            else:
+                odx = self.hash_to_idx[hashed]
+                self.idx_to_count[odx] += 1
 
     def hash_trees(self):
-        "hash ladderized tree topologies"       
+        """
+        hash ladderized tree topologies
+        """
         observed = {}
         for idx, tree in enumerate(self.treelist):
             nwk = tree.write(tree_format=9)
@@ -91,8 +114,9 @@ class ConsensusTree:
 
 
     def map_onto_best_tree(self):
-        "map clades from tree onto best_tree"
-
+        """
+        Map clades from tree onto best_tree
+        """
         # index names from the first tree
         ndict = {j: i for i, j in enumerate(self.names)}
 
@@ -139,21 +163,23 @@ class ConsensusTree:
 
 
     def find_clades(self):
-        "Count clade occurrences."
+        """
+        Count clade occurrences.
+        """
         # index names from the first tree
         ndict = {j: i for i, j in enumerate(self.names)}
-        namedict = {i: j for i, j in enumerate(self.names)}
+        namedict = dict(enumerate(self.names))
 
         # store counts
         clade_counts = {}
-        for tidx, ncopies in self.treedict.items():
+        for tidx, ncopies in self.idx_to_count.items():  #treedict.items():
 
             # testing on unrooted trees is easiest but for some reason slow
-            ttree = self.treelist[tidx].unroot()
+            ttree = self.idx_to_tree[tidx]
 
             # traverse over tree
             for node in ttree.treenode.traverse('preorder'):
-                bits = np.zeros(len(ttree), dtype=np.bool_)
+                bits = np.zeros(len(ttree), dtype=bool)
                 for child in node.iter_leaf_names():
                     bits[ndict[child]] = True
 
@@ -179,11 +205,15 @@ class ConsensusTree:
         self.clade_counts = sorted(
             clade_counts.items(),
             key=lambda x: x[1],
-            reverse=True)
+            reverse=True,
+        )
 
 
     def filter_clades(self):
-        "Remove conflicting clades and those < cutoff to get majority rule"
+        """
+        Remove conflicting clades and those < cutoff to get
+        majority rule.
+        """
         passed = []
         carrs = np.array([list(i[0]) for i in self.clade_counts], dtype=int)
         freqs = np.array([i[1] for i in self.clade_counts])
@@ -213,8 +243,9 @@ class ConsensusTree:
 
 
     def build_trees(self):
-        "Build an unrooted consensus tree from filtered clade counts."
-
+        """
+        Build an unrooted consensus tree from filtered clade counts.
+        """
         # storage
         nodes = {}
         idxarr = np.arange(len(self.fclade_counts[0][0]))
@@ -255,7 +286,7 @@ class ConsensusTree:
 
             # the clade will not be in nodes if it is a tip
             children = [nodes.pop(c) for c in clade if c in nodes]
-            node = TreeNode(name=name)
+            node = toytree.TreeNode(name=name)
             for child in children:
                 node.add_child(child)
             if not node.is_leaf():
@@ -271,11 +302,18 @@ class ConsensusTree:
         tre = nodelist[0]
 
         ## return the tree and other trees if present
-        self.ttree = ToyTree(tre.write(format=0))
+        self.ttree = toytree.tree(tre.write(format=0))
         self.ttree._coords.update()
         self.nodelist = nodelist
 
 
-
 if __name__ == "__main__":
-    pass
+
+    import toytree
+
+    tre1 = toytree.rtree.unittree(10, seed=123)
+    tre2 = toytree.rtree.unittree(10, seed=321)
+
+    mtre = toytree.mtree(([tre1] * 5) + ([tre2] * 5))
+    ctre = mtre.get_consensus_tree()
+    print(ctre.get_node_data())
