8620d221659ef1b33f52ce73b5b728b3dad72779
Deren <de2356@columbia.edu>
2021-08-24 16:07:00 -0400

    module level docstring"

--- toytree/core/__init__.py
+++ toytree/core/__init__.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
 """
-Core Tree objects, as three hierarchical objects:
-	TreeNode
-	Toytree(TreeNode)
-	Multitree([Toytree, Toytree, ...])
+The core subpackage contains modules for manipulating and drawing
+ToyTree and MultiTree objects.
+
+Examples
+--------
+>>> import toytree
+
+Parse tree data to a ToyTree instance:
+>>> tree = toytree.tree("((a,b),c);")
+>>> tree = toytree.tree("https://eaton-lab.org/data/Cyathophora.tre")
+
+Generate random ToyTrees
+>>> tree = toytree.rtree.unittree(ntips=10)
+>>> tree = toytree.rtree.bdtree(ntips=10, b=0.5, d=0.1)
+
+Generate a MultiTree object
+>>> trees = [toytree.rtree.unittree(10) for i in range(5)]
+>>> mtree = toytree.mtree(trees)
+
 """
 
-# import toytree.src.tree
+from . import tree
