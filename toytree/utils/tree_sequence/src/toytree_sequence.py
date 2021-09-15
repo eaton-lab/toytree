be83615018e43bb02fd48ec1014fc5aa77bc42e7
Deren <de2356@columbia.edu>
2021-09-15 13:38:09 -0400

    tree seq use logger, allow multiple roots, update pos->site for ts updates

--- toytree/utils/tree_sequence/src/toytree_sequence.py
+++ toytree/utils/tree_sequence/src/toytree_sequence.py
@@ -18,9 +18,12 @@ Draw the TreeSequence w/ mutations.
 """
 
 from typing import Optional, List, Iterable, Union, Dict
+from loguru import logger
 import numpy as np
 from toytree.utils.tree_sequence.src.draw_tree_seq import TreeSequenceDrawing
 import toytree
+
+logger = logger.bind(name="toytree")
 
 
 class ToyTreeSequence:
@@ -86,7 +89,7 @@ class ToyTreeSequence:
                 size = min(arr.size, self.sample[pidx])
                 samp = self.rng.choice(arr, size=size, replace=False)
                 samps += samp.tolist()
-        sampled_ts = tree_sequence.simplify(samps)
+        sampled_ts = tree_sequence.simplify(samps, keep_input_roots=True)
         return sampled_ts
 
     def at_index(self, index: int):
@@ -100,28 +103,60 @@ class ToyTreeSequence:
         return self._get_toytree(tree, site=pos)
 
     def _get_toytree(self, tree: 'tskit.trees.Tree', site: Optional[int]=None):
-        """Return a ToyTreeMut for a tree selected by site or index.
+        """Return a ToyTree with mutations for a tree selected by site or index.
+
+        It is assumed here that the tree is fully coalesced, returning
+        a multitree for multiple trees present is not yet supported...
         """
+        # create a psuedo-root that will either be replaced by the root
+        # of the tree, if it has a single one, or will be the root to
+        # multipe subtrees if they do not coalesce. This root will have
+        # the root nodes as children, but they will only have None as up
+
         # create the root node
-        pdict = tree.get_parent_dict()        
-        root_idx = max(pdict.values())
-        idx_dict = {root_idx: toytree.TreeNode(name=root_idx, dist=0)}
-        idx_dict[root_idx].add_feature("tsidx", root_idx)
+        tsidx_dict = {}
+
+        # warn use if multiple roots are present
+        if tree.has_multiple_roots:
+            logger.warning(f"tree has multiple ({len(tree.roots)}) roots.")
+            pseudo_root = toytree.TreeNode(name="pseudo-root")
+            pseudo_root.add_feature("tsidx", -1)
+            for tsidx in tree.roots:
+                node = toytree.TreeNode(name=tsidx, dist=tree.branch_length(tsidx))
+                node.add_feature("tsidx", tsidx)
+                node.up = None
+                pseudo_root.children.append(node)
+                tsidx_dict[tsidx] = node
+                logger.debug(f"adding root: {tsidx}")
+
+                # if root has no children then 
+        else:
+            tsidx = tree.roots[0]
+            pseudo_root = toytree.TreeNode(name=tsidx, dist=0)
+            pseudo_root.add_feature("tsidx", tsidx)
+            tsidx_dict[tsidx] = pseudo_root
+
+        # root_idx = max(pdict.values())
+        # idx_dict = {root_idx: toytree.TreeNode(name=root_idx, dist=0)}
+        # idx_dict[root_idx].add_feature("tsidx", root_idx)
 
         # add all children nodes
+        pdict = tree.get_parent_dict()
+        logger.debug(pdict)
         for cidx in pdict:
             pidx = pdict[cidx]
-            if pidx in idx_dict:
-                pnode = idx_dict[pidx]
+            if pidx in tsidx_dict:
+                pnode = tsidx_dict[pidx]
             else:
                 pnode = toytree.TreeNode(
                     name=str(pidx),
                     dist=tree.branch_length(pidx)
                 )
                 pnode.add_feature("tsidx", pidx)
-                idx_dict[pidx] = pnode
-            if cidx in idx_dict:
-                cnode = idx_dict[cidx]
+                tsidx_dict[pidx] = pnode
+                logger.debug(f"adding child: {cidx} to parent: {pidx}")                
+            if cidx in tsidx_dict:
+                cnode = tsidx_dict[cidx]
             else:
                 pop = self.tree_sequence.tables.nodes.population[cidx]
                 cnode = toytree.TreeNode(
@@ -129,16 +164,16 @@ class ToyTreeSequence:
                     dist=tree.branch_length(cidx),
                 )
                 cnode.add_feature("tsidx", cidx)
-                idx_dict[cidx] = cnode
+                tsidx_dict[cidx] = cnode
             cnode.up = pnode
             pnode.children.append(cnode)
 
         # wrap TreeNode as a toytree
-        ttree = toytree.ToyTree(idx_dict[root_idx])#.ladderize()
+        ttree = toytree.ToyTree(pseudo_root)#root_idx])#.ladderize()
 
         # add mutations as metadata
         if site is not None:
-            ttree.mutations = [i for i in tree.mutations() if i.position == site]
+            ttree.mutations = [i for i in tree.mutations() if i.site == site]
         else:
             ttree.mutations = list(tree.mutations())
 
@@ -253,7 +288,7 @@ class ToyTreeSequence:
             colors.append(color)
             title = (
                 f"id: {mut.id}\n"
-                f"pos: {mut.position:.0f}\n"
+                f"site: {mut.site:.0f}\n"
                 f"time: {mut.time:.0f}\n"
                 f"mtype: {mtype}")
             titles.append(title)
@@ -285,20 +320,25 @@ if __name__ == "__main__":
     # EXAMPLE
     import ipcoal
     import toyplot.browser
+    import pyslim
+    toytree.set_log_level("DEBUG")
 
-    TREE = toytree.rtree.unittree(10, treeheight=1e5)
-    MOD = ipcoal.Model(tree=TREE, Ne=1e4, nsamples=1)
-    MOD.sim_loci(5, 2000)
+    slim_ts = pyslim.load("/tmp/test2.trees")
+    tts = ToyTreeSequence(slim_ts, sample=5, seed=123).at_index(0)
+
+    # TREE = toytree.rtree.unittree(10, treeheight=1e5)
+    # MOD = ipcoal.Model(tree=TREE, Ne=1e4, nsamples=1)
+    # MOD.sim_loci(5, 2000)
     
-    # optional: access the tree sequences directly
-    TS = MOD.ts_dict[0]
+    # # optional: access the tree sequences directly
+    # TS = MOD.ts_dict[0]
 
-    # get a specific ToyTree
-    TOYTS = ToyTreeSequence(TS)
+    # # get a specific ToyTree
+    # TOYTS = ToyTreeSequence(TS)
 
-    # draw the tree sequence
-    TOYTS.draw(width=None, height=300, idxs=None, scrollable=True)
+    # # draw the tree sequence
+    # # TOYTS.draw_tree(width=None, height=300, idxs=None, scrollable=True)
 
-    # draw one tree
-    TOY = TOYTS.at_index(0)
-    TOY.draw()
+    # # draw one tree
+    # TOY = TOYTS.at_index(0)
+    # print(TOY)
