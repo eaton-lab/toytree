41f565cad2b9288cbdeb695d305bb67fb8187a66
Deren <de2356@columbia.edu>
2021-12-22 16:06:04 -0500

    subpackage structure updates

--- toytree/__init__.py
+++ toytree/__init__.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
-"""
-A minimalist Python package for visualizing and studying evolution on trees.
+"""A minimalist Python package for visualizing and studying evolution 
+on trees.
+
+Toytree's primary use if for visualizing and manipulating tree data
+structures. It includes a number of additional subpackages for working
+with trees as data, or data on trees. All subpackages make use only of
+standard Python data science libs (e.g., numpy, scipy, pandas, numba) 
+and does not include wrappers around any external tools.
+
+Examples
+--------
+>>> tree1 = toytree.rtree.unittree(ntips=10)
+>>> tree1.draw();
+>>> tree2 = toytree.tree("https://eaton-lab.org/data/Cyathophora.tree")
+>>> tree2.root(wildcard="prz").draw(tree_style='o')
 """
 
-__version__ = "2.1.1-dev"
+__version__ = "3.0-dev"
 __author__ = "Deren Eaton"
 
 
 # expose core classes (TreeNode, ToyTree, MultiTree)
 # and factory functions (.tree, .rtree, .mtree, .rawtree)
-from toytree.core.tree import tree, ToyTree
-from toytree.core.rawtree import RawTree as rawtree
-from toytree.core.treenode import TreeNode
-from toytree.core.multitree import mtree, MultiTree
+# from toytree.core.tree import tree, ToyTree
+# from toytree.core.rawtree import RawTree as rawtree
+# from toytree.core.treenode import TreeNode
+# from toytree.core.multitree import mtree, MultiTree
 
 # submodules are exposed with curated functions in their __init__
 # from toytree.pcm import ...
@@ -22,13 +35,25 @@ from toytree.core.multitree import mtree, MultiTree
 # from toytree.annotate import ...
 
 # should these be made into top-level submodules?
-from toytree.core import rtree
-# from toytree.style import ...
+# from toytree.core import rtree
+
+# toytree v3 supported modules
+from toytree.core.node import Node
+from toytree.core.tree2 import ToyTree
+from toytree.core.io.treeio import tree
+# from toytree.core.multitree import MultiTree, mtree
+
+import toytree.rtree
+import toytree.distance
+# import toytree.io
+# import toytree.color
+# import toytree.distance
+# import toytree.pcm
+# import toytree.mod
+# import toytree.annotate
 
 # from toytree.core.style.color import ToyColor
-from toytree.core.style.color import COLORS1, COLORS2, color_cycler
-# expose submodules
-# import toytree.pcm
+# from toytree.core.style.color import COLORS1, COLORS2, color_cycler
 
 # start the logger in INFO
 from toytree.utils.src.logger_setup import set_log_level
