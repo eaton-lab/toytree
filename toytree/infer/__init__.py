49a366e8cf219976c7489e42c1b9535b95ac29d9
Deren <de2356@columbia.edu>
2022-02-27 15:49:15 -0500

    distance tree inference functions

---
+++ toytree/infer/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""toytree inference subpackage.
+
+
+"""
+
+from toytree.infer.src.upgma import infer_upgma_tree
+from toytree.infer.src.neighbor_joining import infer_neighbor_joining_tree
