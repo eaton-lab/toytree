9550e39da28a9e7132e8bd695ed01f1bc0aef27c
Deren <de2356@columbia.edu>
2022-03-03 18:20:46 -0500

    new normalize values for Series inputs

--- toytree/utils/transform.py
+++ toytree/utils/transform.py
@@ -3,20 +3,29 @@
 """Data transformations for formatting node values nicely.
 """
 
-
 from copy import deepcopy
 import numpy as np
+import pandas as pd
+from numpy.typing import ArrayLike
 from toytree.utils import ToytreeError
 
 
-def normalize_values(values, nbins=10, min_value=2, max_value=12):
+def normalize_values(
+    values: pd.Series,
+    nbins: int=10,
+    min_value: int=2,
+    max_value: int=12,
+    ) -> ArrayLike:
     """Distribute values into bins spaced at reasonable sizes for plotting.
 
     This is used in tree_style='p' to automatically scale Ne values
-    to plot as edge widths.
+    to plot as edge widths. The input 'values' arg will usually be
+    a pandas.Series.
+
+    TODO: get_node_data dtype setting..
     """
     # missing values are not allowed
-    if np.isnan(np.array(values)).any():
+    if values.isna().any():
         raise ToytreeError(
             f"missing values are not allowed:\n{values}.")
 
@@ -42,3 +51,20 @@ def normalize_values(values, nbins=10, min_value=2, max_value=12):
         for _ in range(inbin):
             newvals[svals.pop(0)] = sizes[idx]
     return np.array([newvals[i] for i in ovals])
+
+
+if __name__ == "__main__":
+
+    import ipcoal
+    import toytree
+
+    # generate a random species tree with 10 tips and a crown age of 10M generations
+    tree = toytree.rtree.unittree(10, treeheight=1e6, seed=123)
+    # create a new tree copy with Ne values mapped to nodes
+    vtree = tree.set_node_data(
+        feature="Ne",
+        mapping={i: 2e5 for i in (6, 7, 8, 9, 12, 15, 17)},
+        default=1e4,
+    )
+
+    vtree._draw_browser(ts='p', admixture_edges=[(0, 12, 0.5, {'stroke': 'red'}, "hello")]);
