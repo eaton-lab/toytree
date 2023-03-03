b8b9c54b0b6c6f99e9f73de0539ead1c1baf1623
Deren <de2356@columbia.edu>
2023-03-03 10:03:24 -0500

    removed colorkit custom repr for simpler code.

--- toytree/color/src/colorkit.py
+++ toytree/color/src/colorkit.py
@@ -2,29 +2,21 @@
 
 """ColorKit class for parsing and converting between color formats.
 
-This is used to parse a SINGLE color input. This is an internal 
-not intended to be used by users. Instead, users should use the 
-ToyColor object, which uses ColorKit as its parser.
-
-Note
-----
-If the color value being parsed can be either a single color or a 
-container object with multiple colors then the `color_expander()` 
-function should be used to parse it. That func will return either a 
-single ToyColor or List[ToyColor].
+This is used to parse a SINGLE color input. This is an internal func
+not intended for users. Instead, users should use the ToyColor object,
+which uses ColorKit as its parser.
 """
 
 from typing import Union, Tuple
-import xml.etree.ElementTree as xml
 from copy import copy
 import numpy as np
 from loguru import logger
 import toyplot.color
-from toytree.color.src.utils import style_to_string
-from toytree.utils import ToytreeError
+from toytree.utils import ToyColorError
 
 logger = logger.bind(name="toytree")
 ColorType = Union[str, np.ndarray, Tuple[float, float, float, float]]
+
 
 class ColorKit:
     """Flexible color parser class to get css or rgba tuples.
@@ -57,7 +49,7 @@ class ColorKit:
     @property
     def css(self) -> str:
         """Return CSS string as 'rgba(R%,G%,B%,A%)'."""
-        return self._css        
+        return self._css
 
     @property
     def array(self) -> np.ndarray:
@@ -75,27 +67,46 @@ class ColorKit:
         return tuple(float(self.array[i]) for i in 'rgb')
 
     def get_style_str(self, stroke: bool = False, opacity: float = None) -> str:
-        """Return SVG style string with fill/stroke and opacity separate."""
+        """Return SVG style string with fill/stroke and opacity separate.
+
+        If opacity is set to False then no opacity is printed. This is
+        used when the parent class has an opacity that should be
+        inherited.
+        """
+        # choose stroke or fille
+        coltype = "stroke" if stroke else "fill"
+        # get rbg from rgba
         rgb_css = f"rgb({self.css[5:].rsplit(',', 1)[0]})"
-        opacity = self.rgba[-1] if opacity is None else opacity
-        if stroke:
-            return f"stroke:{rgb_css};stroke-opacity:{opacity}"
-        return f"fill:{rgb_css};fill-opacity:{opacity}"
+        # store rgb color as string
+        color = f"{coltype}:{rgb_css}"
+        # if opacity is None then get value from rgba
+        if opacity is None:
+            opacity = f";{coltype}-opacity:{self.rgba[-1]}"
+        # if opacity if False then do not set.
+        elif opacity is False:
+            opacity = ""
+        # if opacity if a value then use it.
+        else:
+            opacity = f";{coltype}-opacity:{opacity}"
+        # return style string
+        return color + opacity
 
     def _parse_color(self, color: ColorType) -> None:
         """Parse input color to three stored formats."""
         # input is a css string (parse input based on type
+
+        # logger.error(color)
         if isinstance(color, str):
             self._array = toyplot.color.css(color)
             if self._array is None:
-                raise ToytreeError(f"CSS color '{color}' not recognized")
+                raise ToyColorError(f"CSS color '{color}' not recognized")
             self._css = toyplot.color.to_css(self._array)
             self._rgba = tuple(float(self._array[i]) for i in 'rgba')
 
         # input is an ndarray (e.g., toyplot.color ndarray, 1-d 4 floats)
         elif isinstance(color, np.ndarray) and color.dtype == toyplot.color.dtype:
             if color.size > 1:
-                raise ToytreeError(
+                raise ToyColorError(
                     "Cannot parse a multi-color array to a ToyColor. "
                     "Use `toytree.color.ToyColor.color_expander()`\n"
                     "to convert it to a List[ToyColor]."
@@ -103,8 +114,8 @@ class ColorKit:
             self._array = color.copy()
             self._css = toyplot.color.to_css(color)
             if not self._css:
-                raise ToytreeError(f"color array {color} not recognized")
-            self._rgba = tuple(float(self._array[i]) for i in 'rgba')                
+                raise ToyColorError(f"color array {color} not recognized")
+            self._rgba = tuple(float(self._array[i]) for i in 'rgba')
 
         elif isinstance(color, tuple) and len(color) == 4:
             self._rgba = color
@@ -126,27 +137,39 @@ class ColorKit:
             self._array = toyplot.color.rgba(*self._rgba)
             self._css = toyplot.color.to_css(self._array)
 
+        elif isinstance(color, float):
+            if np.isnan(color):
+                self._rgba = (0, 0, 0, 0)
+                self._array = toyplot.color.rgba(*self._rgba)
+                self._css = toyplot.color.to_css(self._array)
+            else:
+                raise ToyColorError(
+                    f"Color arg '{color}' not recognized as a valid color input.")
         else:
-            raise ToytreeError(f"color arg {color} not supported")
+            raise ToyColorError(
+                f"Color arg '{color}' not recognized as a valid color input.")
 
-    def _repr_html_(self):
-        """Show color as a div in jupyter notebooks"""
-        # create a root dom element
-        root_xml = xml.Element(
-            "div",
-            style="overflow:hidden; height:auto; display:table",
-            attrib={"class": "toytree-ToyColor"},
-        )
-        _ = xml.SubElement(root_xml, "div", style=style_to_string(self._style),
-        )
-        _ = xml.SubElement(root_xml, "text").text = (
-            "Color({:.2f}, {:.2f}, {:.2f}, {:.2f})"
-            .format(*self.rgba)
-        )
+    # Colorkit doesn't need a repr, but this one was pretty nice, so 
+    # I'll leave it here for now. But I commented it b/c I was encountering
+    # a circular import error and didn't feel like fixing it.
+    # def _repr_html_(self):
+    #     """Show color as a div in jupyter notebooks"""
+    #     # create a root dom element
+    #     root_xml = xml.Element(
+    #         "div",
+    #         style="overflow:hidden; height:auto; display:table",
+    #         attrib={"class": "toytree-ToyColor"},
+    #     )
+    #     _ = xml.SubElement(root_xml, "div", style=concat_style_to_str(self._style))
+    #     _ = xml.SubElement(root_xml, "text").text = (
+    #         "Color({:.2f}, {:.2f}, {:.2f}, {:.2f})"
+    #         .format(*self.rgba)
+    #     )
 
-        # convert deom to a simple html script
-        html = xml.tostring(root_xml, encoding="unicode", method="html")
-        return html
+    #     # convert deom to a simple html script
+    #     html = xml.tostring(root_xml, encoding="unicode", method="html")
+    #     return html
+
 
 if __name__ == "__main__":
 
@@ -157,7 +180,7 @@ if __name__ == "__main__":
         "rgba(100.0%,50.0%,25.0%,0.500)",
         "rgb(100%,50%,25%)",
         (1.0, 0.5, 0.25, 0.5),
-        (1.0, 0.5, 0.25),        
+        (1.0, 0.5, 0.25),
         np.array((1.0, 0.5, 0.25, 0.5), dtype=toyplot.color.dtype),
         ColorKit("red"),
     ]
