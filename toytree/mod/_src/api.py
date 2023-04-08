313fa1e315f29d92085e1dfd4a35dda9616e4726
Deren <de2356@columbia.edu>
2023-04-08 08:42:24 -0400

    move some error msgs

--- toytree/mod/_src/api.py
+++ toytree/mod/_src/api.py
@@ -1,272 +1,60 @@
 #!/usr/bin/env python
 
-"""API class for accessing `mod` subpackage functions from ToyTrees.
-
-The `TreeModAPI` can be accessed either at the package level as
-`toytree.mod` or from the object level as `ToyTree.mod`. In the latter
-case the default argument to all functions is the ToyTree object from
-which the function is called.
-
-Examples
---------
->>> tree = toytree.rtree.unittree(10)
->>> toytree.mod.unroot(tree)
->>> tree.mod.unroot()
 """
 
-from typing import Dict, Optional, TypeVar, Sequence
-import inspect
-import toytree.mod
-from toytree.mod._src.mod_edges import (
-    edges_scale_to_root_height,
-    edges_slider,
-    edges_multiplier,
-    edges_set_node_heights,
-    edges_extend_tips_to_align,
-)
-from toytree.mod._src.mod_topo import (
-    ladderize,
-    collapse_nodes,
-    remove_unary_nodes,
-    rotate_node,
-    extract_subtree,
-    prune,
-    drop_tips,
-    resolve_polytomies,
+"""
 
-    add_child_node,
-    add_sister_node,
-    add_internal_node,
-    add_internal_node_and_child,
-    add_internal_node_and_subtree,
-)
-from toytree.mod._src.root_unroot import (
-    unroot,
-    root,
-)
-from toytree.mod._src.root_funcs import (
-    root_on_midpoint,
-    root_on_minimal_ancestor_deviation,
-)
-# from toytree import Node
-# from toytree.mod.penalized_likelihood import Chronos
+from typing import TypeVar
+from functools import wraps
 
-Node = TypeVar("Node")
 ToyTree = TypeVar("ToyTree")
-Query = TypeVar("Query", str, int, Node)
 
 
 class TreeModAPI:
-    """ToyTree modify tree topology API.
-
-    Accessible from ToyTree class objects at .mod.[function].
-    """
+    """API to acess methods from `toytree.mod` as `tree.mod.{function}`"""
     def __init__(self, tree: ToyTree):
         self._tree = tree
-        """Refto a ToyTree instance, hidden for API tab-complete."""
-
-    def edges_scale_to_root_height(
-        self,
-        treeheight: float = 1.,
-        include_stem: bool = False,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod._src.mod_edges"""
-        return edges_scale_to_root_height(self._tree, treeheight, include_stem, inplace)
-
-    def edges_slider(
-        self,
-        prop: float = 0.999,
-        seed: Optional[int] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod._src.mod_edges"""
-        return edges_slider(self._tree, prop, seed, inplace)
-
-    def edges_multiplier(
-        self,
-        multiplier: float = 1.0,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod._src.mod_edges"""
-        return edges_multiplier(self._tree, multiplier=multiplier, inplace=inplace)
-
-    def edges_extend_tips_to_align(self, inplace: bool = False) -> ToyTree:
-        """Docstring inherited from toytree.mod._src.mod_edges"""
-        return edges_extend_tips_to_align(self._tree, inplace=inplace)
-
-    def edges_set_node_heights(
-        self, mapping: Dict[Query, float], inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod._src.mod_edges"""
-        return edges_set_node_heights(self._tree, mapping=mapping, inplace=inplace)
-
-    def ladderize(self, direction: bool = False, inplace: bool = False) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return ladderize(self._tree, direction=direction, inplace=inplace)
-
-    def collapse_nodes(
-        self,
-        *query: Query,
-        min_dist: float = 1e-6,
-        min_support: float = 0,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return collapse_nodes(self._tree, *query, min_dist=min_dist, min_support=min_support, inplace=inplace)
-
-    def remove_unary_nodes(self, inplace: bool = False) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return remove_unary_nodes(self._tree, inplace=inplace)
-
-    def rotate_node(self, *query: Query, regex: bool = False, inplace: bool = False) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return rotate_node(self._tree, *query, regex=regex, inplace=inplace)
-
-    def extract_subtree(self, *query: Query, regex: bool = False) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return extract_subtree(self._tree, *query, regex=regex)
-
-    def prune(
-        self,
-        *query: Query,
-        regex: bool = False,
-        preserve_branch_length: bool = True,
-        require_root: bool = True,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return prune(
-            self._tree, *query, regex=regex, preserve_branch_length=preserve_branch_length,
-            require_root=require_root, inplace=inplace)
-
-    def drop_tips(
-        self,
-        *query: Query,
-        regex: bool = False,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return drop_tips(self._tree, *query, regex=regex, inplace=inplace)
-
-    def resolve_polytomies(
-        self,
-        *query: Query,
-        regex: bool = False,
-        dist: float = 1.0,
-        support: float = 100,
-        recursive: bool = True,
-        seed: Optional[int] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring inherited from toytree.mod."""
-        return resolve_polytomies(
-            self._tree, *query, regex=regex, dist=dist, support=support,
-            recursive=recursive, seed=seed, inplace=inplace)
-
-    def add_internal_node(
-        self,
-        *query: Query,
-        regex: bool = False,
-        dist: Optional[float] = None,
-        name: Optional[str] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is copied from mod_topo.py"""
-        return add_internal_node(self._tree, *query, regex=regex, dist=dist, name=name, inplace=inplace)
-
-    def add_child_node(
-        self,
-        *query: Query,
-        regex: bool = False,
-        name: Optional[str] = None,
-        dist: Optional[float] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is copied from mod_topo.py"""
-        return add_child_node(self._tree, *query, regex=regex, name=name, dist=dist, inplace=inplace)
-
-    def add_sister_node(
-        self,
-        *query: Query,
-        regex: bool = False,
-        name: Optional[str] = None,
-        dist: Optional[float] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is copied from mod_topo.py"""
-        return add_sister_node(self._tree, *query, regex=regex, name=name, dist=dist, inplace=inplace)
-
-    def add_internal_node_and_child(
-        self,
-        *query: Query,
-        regex: bool = False,
-        name: Optional[str] = None,
-        dist: Optional[float] = None,
-        parent_dist: Optional[float] = None,
-        parent_name: Optional[str] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is copied from mod_topo.py"""
-        return add_internal_node_and_child(
-            self._tree, *query, regex=regex, name=name, dist=dist, parent_dist=parent_dist,
-            parent_name=parent_name, inplace=inplace)
-
-    def add_internal_node_and_subtree(
-        self,
-        *query: Query,
-        regex: bool = False,
-        subtree: ToyTree,
-        subtree_stem_dist: Optional[float] = None,
-        subtree_rescale: bool = False,
-        parent_dist: Optional[float] = None,
-        parent_name: Optional[str] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is copied from mod_topo.py"""
-        return add_internal_node_and_subtree(
-            self._tree, *query, regex=regex, subtree=subtree, subtree_stem_dist=subtree_stem_dist,
-            subtree_rescale=subtree_rescale, parent_dist=parent_dist, parent_name=parent_name, inplace=inplace)
-
-    ###################################################################
-    def root(
-        self,
-        *query: Query,
-        regex: bool = False,
-        root_dist: Optional[float] = None,
-        edge_features: Optional[Sequence[str]] = None,
-        inplace: bool = False,
-    ) -> ToyTree:
-        """Docstring is inherited from toytree.mod._src.root_unroot.py"""
-        return root(
-            self._tree, *query, regex=regex, root_dist=root_dist,
-            edge_features=edge_features, inplace=inplace
-        )
-
-    def unroot(self, inplace: bool = False) -> ToyTree:
-        """Docstring is inherited from toytree.mod._src.root_unroot.py"""
-        return unroot(self._tree, inplace=inplace)
-
-    def root_on_midpoint(self, inplace: bool = False) -> ToyTree:
-        """Docstring is inherited from toytree.mod._src.root_unroot.py"""
-        return root_on_midpoint(self._tree, inplace=inplace)
-
-    def root_on_minimal_ancestor_deviation(self, *query: Query, inplace: bool = False, return_stats: bool = False) -> ToyTree:
-        """Docstring is inherited"""
-        return root_on_minimal_ancestor_deviation(self._tree, *query, inplace=inplace, return_stats=return_stats)
 
 
-########################################################################
-# COPY DOCUMENTATION STRINGS TO API FUNCS FROM THE MODULE-LEVEL FUNCS
-########################################################################
-for name, module_func in inspect.getmembers(toytree.mod):
-    if inspect.isfunction(module_func):
+def add_method(cls):
+    """Clever approach to copy docstring and signature from func to API.
 
-        api_func = getattr(TreeModAPI, name)
-        api_func.__doc__ = module_func.__doc__
-########################################################################
-########################################################################
-########################################################################
+    Reference
+    ---------
+    https://mgarod.medium.com/dynamically-add-a-method-to-a-class-in-python-c49204b85bd6
+    """
+    def decorator(func):
 
-if __name__ == "__main__":
-    help(TreeModAPI.root)
+        # creates a wrapper for a toytree.mod.{function} that copies
+        # its docstring and arg signatures but sets self._tree as the
+        # first argument of the function.
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            return func(self._tree, *args, **kwargs)
+
+        # sets: TreeModAPI.{function} = wrapper
+        setattr(cls, func.__name__, wrapper)
+
+        # Note we are not binding func, but wrapper which accepts self
+        # but does exactly the same as func.
+        # returning func means func can still be used normally
+        return func
+    return decorator
+
+
+# ########################################################################
+# # COPY DOCUMENTATION STRINGS TO API FUNCS FROM THE MODULE-LEVEL FUNCS
+# # this code will run always on init.
+# ########################################################################
+# for name, module_func in inspect.getmembers(toytree.mod):
+#     if inspect.isfunction(module_func):
+
+#         @add_method(TreeModAPI)
+
+
+#         # set documentation strings to match
+#         api_func = getattr(TreeModAPI, name)
+#         api_func.__doc__ = module_func.__doc__
+
+#         # set default args to match
+#         setattr(api_func, func.__name__, wrapper)
--- toytree/utils/src/exceptions.py
+++ toytree/utils/src/exceptions.py
@@ -56,6 +56,36 @@ class NexusError(ToytreeError):
     pass
 
 
+NODE_NOT_IN_TREE_ERROR = """\
+"query Node belongs to a different tree (e.g., tree1) than the one
+being queried (e.g., tree2). Make sure you are selecting the correct
+Node, and consider using Node .idx or .name features as queries. Beware
+.idx labels often change between trees. To avoid this warning try the
+following to translate a query from one tree to another:
+
+# get Node of interest from tree1 using your query
+>>> node = tree1.get_mrca_node(*query)
+
+# represent this Node by its set of leaf names
+>>> tips = node.get_leaf_names()
+
+# run your function of interest on tree2 using leaf names as query
+>>> tree2.get_nodes(*tips)
+"""
+
+NODE_INDEXING_ERROR = """\
+Error indexing from ToyTree object. You likely entered an invalid
+Node query. Note: using indexing Nodes can be selected by their
+int idx label by index, slicing, or as a list:
+>>> tree[3]                           # Node(idx=3)
+>>> tree[3:4]                         # [Node(idx=3), Node(idx=4)]
+>>> tree[[3,10,2]                     # [Node(idx=3), Node(idx=10), Node(idx=2)]
+
+Also, see the `.get_nodes` function for more options to query Nodes
+by int idx label, str name label, Node object, or a mixture:
+>>> tree.get_nodes('name3', 0)        # [Node(idx=3), Node(idx=0)]
+"""
+
 if __name__ == "__main__":
 
     # raise ValueError("HELLO WORLD")
