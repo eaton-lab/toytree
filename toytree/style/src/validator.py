9d174fcaaef1ab89be8ceb83b8383545067dc9a9
Deren <de2356@columbia.edu>
2023-04-11 14:57:26 -0400

    move color mapper

--- toytree/style/src/validator.py
+++ toytree/style/src/validator.py
@@ -15,24 +15,10 @@ from toytree.utils import (
     ToytreeError,
     StyleSizeMismatchError,
     StyleTypeMismatchError,
-    StyleColorMappingTupleError,
 )
 
 logger = logger.bind(name="toytree")
 ToyTree = TypeVar("ToyTree")
-ColorMap = Union[str, toyplot.color.Map]
-
-CMAP_ERROR = """
-To find a list of valid colormap names supported by toyplot use:
->>> print(toyplot.color.brewer.names())
-
-You can map feature values to colors by entering (feature, colormap):
->>> tree.draw('c', node_colors=('dist', 'Spectral'))
-
-or, load a colormap as a Map object for finer control:
->>> cmap = toyplot.color.brewer.map('BlueRed', domain_min=0, domain_max=1)
->>> tree.draw('c', node_colors=('dist', cmap))\
-"""
 
 
 def check_arr(values: Sequence[Any], label: str, size: int, ctype: type) -> np.ndarray:
@@ -51,42 +37,6 @@ def check_arr(values: Sequence[Any], label: str, size: int, ctype: type) -> np.n
             f"'{label}' type not supported. You entered {type(arr[0])}, "
             f"should be len={ctype}.")
     return arr
-
-
-def get_color_mapped_feature(values: np.ndarray, cmap: ColorMap) -> np.ndarray:
-    """Return feature mapped to a continuous or discrete color map.
-
-    Raises helpful error messages if user entered the special tuple
-    argument incorrectly, or by accident.
-    """
-    # select map from str name and check that Map is valid.
-    if isinstance(cmap, str):
-        try:
-            cmap = toyplot.color.brewer.map(cmap)
-        except KeyError as inst:
-            raise StyleColorMappingTupleError(
-                f"'{cmap}' is an invalid colormap argument for the "
-                "special color mapping syntax entered as a tuple, e.g., "
-                "(feature, colormap).\n" + CMAP_ERROR
-            ) from inst
-    if not isinstance(cmap, toyplot.color.Map):
-        raise StyleColorMappingTupleError(
-            f"'{cmap}' is invalid for the special tuple arg type "
-            "(feature, colormap)." + CMAP_ERROR
-        )
-
-    # auto-set domains to min and max values if not set on cmap.
-    if cmap.domain.min is None:
-        cmap.domain.min = np.nanmin(values)
-    if cmap.domain.max is None:
-        cmap.domain.max = np.nanmax(values)
-
-    # broadcast values to color map
-    colors = cmap.colors(values)
-
-    # set colors for nan values to "transparent"
-    colors[np.isnan(values)] = ToyColor((0, 0, 0, 0))
-    return colors
 
 
 def validate_style(tree: ToyTree, style: TreeStyle) -> TreeStyle:
@@ -133,7 +83,6 @@ class Validator(TreeStyle):
         ----
         None
         ToyColor, str (css)
-
         """
         # if no node_colors then nodes will be colored by node_style.fill
         if self.node_colors is None:
