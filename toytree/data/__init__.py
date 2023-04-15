55b5e82273c4531d92f2c4d069626fa5168b600a
Deren <de2356@columbia.edu>
2023-04-15 19:13:40 -0400

    new data subpkg

--- toytree/data/__init__.py
+++ toytree/data/__init__.py
@@ -14,4 +14,7 @@ toytree.data.distance_matrix
 toytree.data.sequence_alignment
 """
 
-from toytree.data._src.transform import normalize_values
+from toytree.data._src.get_node_data import get_node_data
+from toytree.data._src.set_node_data import set_node_data
+from toytree.data._src.expand_node_mapping import expand_node_mapping
+# from toytree.data._src.transform import normalize_values
