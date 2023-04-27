753890d177bfec467c7713e3a088593e3c867b44
Deren <de2356@columbia.edu>
2023-04-27 11:04:27 -0400

    cosmetic

--- toytree/drawing/__init__.py
+++ toytree/drawing/__init__.py
@@ -5,15 +5,16 @@
 """
 
 from toytree.drawing.src.toytree_mark import ToyTreeMark
-from toytree.drawing.src.canvas_setup import (
-    CanvasSetup,
-    GridSetup,
+from toytree.drawing.src.setup_grid import GridSetup
+from toytree.drawing.src.setup_canvas import (
+    get_canvas_and_axes,
     set_axes_ticks_style,
+    CanvasSetup,
 )
 from toytree.drawing.src.draw_toytree import (
     draw_toytree,
     get_layout,
-    get_tree_style,
+    get_tree_style_base,
 )
 from toytree.drawing.src.render_text import render_text
 import toytree.drawing.src.render_tree
--- toytree/drawing/src/__init__.py
+++ toytree/drawing/src/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 
 """..."""
+from .cartesian import Cart
+from .cartesian_funcs import *
