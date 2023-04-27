9031ca2422d3721175c4b525124501a67137e5e1
Deren <de2356@columbia.edu>
2023-04-27 10:44:51 -0400

    reorganize style validation

--- toytree/style/__init__.py
+++ toytree/style/__init__.py
@@ -2,11 +2,22 @@
 
 """..."""
 
-from toytree.style.src.tree_style_base import TreeStyle
-from toytree.style.src.sub_styles import SubStyle
-from toytree.style.src.tree_styles import get_base_tree_style_by_name
-from toytree.style.src.normalize_values import normalize_values
-from toytree.style.src.validator import validate_style
+# from toytree.style.src.tree_style_base import TreeStyle
+# from toytree.style.src.sub_styles import SubStyle
+# from toytree.style.src.validator import validate_style
+# from toytree.style.src.map_colors import get_color_mapped_values
+# from toytree.style.src.map_values import get_range_mapped_values
+
+from toytree.style.src.style_base import TreeStyle, SubStyle
+from toytree.style.src.style_types import get_base_tree_style_by_name
+from toytree.style.src.map_colors import (
+    get_color_mapped_feature, get_color_mapped_values)
+from toytree.style.src.map_values import (
+    get_range_mapped_feature, get_range_mapped_values)
+from toytree.style.src.validate_style import validate_style
+from toytree.style.src.validate_utils import (
+    tree_style_to_css_dict, substyle_dict_to_css_dict, check_arr)
+
 
 if __name__ == "__main__":
 
