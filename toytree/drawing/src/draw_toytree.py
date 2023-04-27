7a831722c1e11eba7b0386e391b9f9834967958f
Deren <de2356@columbia.edu>
2023-04-27 10:48:44 -0400

    update for new TreeStyle

--- toytree/drawing/src/draw_toytree.py
+++ toytree/drawing/src/draw_toytree.py
@@ -9,26 +9,27 @@ The `draw_toytree` func does the following:
     - ToyTreeMark to generate and return the tree drawing.
 """
 
-from typing import Tuple, TypeVar
-from copy import deepcopy
+from typing import Tuple, TypeVar, Optional
 from loguru import logger
 from toyplot.canvas import Canvas
 from toyplot.coordinates import Cartesian
-from toytree.layout import (
-    BaseLayout, LinearLayout, CircularLayout, UnrootedLayout)
-from toytree.drawing import ToyTreeMark, CanvasSetup
+
+from toytree.drawing import ToyTreeMark, get_canvas_and_axes, CanvasSetup
+from toytree.layout import BaseLayout, LinearLayout, CircularLayout, UnrootedLayout
 from toytree.style import (
-    TreeStyle, SubStyle, get_base_tree_style_by_name)
-from toytree.style.src.validator import validate_style
+    TreeStyle,
+    validate_style,
+    get_base_tree_style_by_name,
+    tree_style_to_css_dict,
+)
 
 
 # from toytree.utils import ToytreeError
-
 ToyTree = TypeVar("ToyTree")
 logger = logger.bind(name="toytree")
 
 
-def draw_toytree(**kwargs) -> Tuple[Canvas, Cartesian, ToyTreeMark]:
+def draw_toytree(tree: ToyTree, **kwargs) -> Tuple[Canvas, Cartesian, ToyTreeMark]:
     """Parse arguements to draw function and return drawing objects.
 
     The drawing style arguments can be entered in two ways, either
@@ -44,12 +45,13 @@ def draw_toytree(**kwargs) -> Tuple[Canvas, Cartesian, ToyTreeMark]:
     class init function, along with the edges and verts.
     """
     # extract the ToyTree instance from kwargs
-    tree = kwargs.pop("toytree")
+    # TODO: can we get rid of this?
+    # tree = kwargs.pop("toytree")
 
-    # extract optional non-styling kwargs
+    # extract Cartesian axes or None
     axes = kwargs.pop("axes")
 
-    # extract extra kwargs that are not in .draw func
+    # extract extra kwargs that are not in ToyTree.draw()
     extra_kwargs = kwargs.pop("kwargs")
 
     # remove 'ts' shortcut name and expand as a user tree_style arg
@@ -63,10 +65,18 @@ def draw_toytree(**kwargs) -> Tuple[Canvas, Cartesian, ToyTreeMark]:
             "\nCheck the docs, argument names may have changed."
         )
 
-    # get a TreeStyle (as new basestyle or tree.style) and update with draw kwargs
-    style = get_tree_style(tree, **kwargs)
+    # get a TreeStyle copy from tree.style or new ts TreeStyle()
+    style = get_tree_style_base(tree, tree_style=kwargs.pop('tree_style'))
+
+    # check and expand user-kwargs if provided else base style value
+    style = validate_style(tree, style, **kwargs)
 
     # get a Layout with coordinates projected based on style
+    # also sets tip_labels_angles for circular and unrooted layouts.
+    # if no user value to tip-labels-angles then get layout generated val.
+    # TODO: move to validation.
+    # if style.tip_labels_angles is None:
+    #     style.tip_labels_angles = layout.angles
     layout = get_layout(
         tree=tree,
         style=style,
@@ -74,78 +84,51 @@ def draw_toytree(**kwargs) -> Tuple[Canvas, Cartesian, ToyTreeMark]:
         fixed_position=kwargs.pop("fixed_position"),
     )
 
-    # if no user value to tip-labels-angles then use layout generated val.
-    if style.tip_labels_angles is None:
-        style.tip_labels_angles = layout.angles
+    # generate toyplot Mark. Style is already validated. tables of int idx labels
+    mark = ToyTreeMark(
+        ntable=layout.coords,
+        etable=tree.get_edges('idx'),
+        **tree_style_to_css_dict(style),
+    )
 
-    # check all styles and expand in-place to array values for all nodes
-    validate_style(tree, style)
-
-    # get canvas and axes
+    # use existing canvas and axes or create new ones. If created, the
+    # size is built from (extents, ntips, height, margin, padding)
+    # TODO: update. requires validated tip labels; sets height, width, ...
+    logger.warning(style.height)
+    # canvas, axes = get_canvas_and_axes(tree, axes, style)
     csetup = CanvasSetup(tree, axes, style)
     canvas = csetup.canvas
     axes = csetup.axes
-
-    # generate toyplot Mark. Style is already validated. tables of int idx labels
-    mark = ToyTreeMark(
-        ntable=layout.coords, etable=tree.get_edges('idx'), **style.dict(serialize=False)
-    )
 
     # add mark to axes
     axes.add_mark(mark)
     return canvas, axes, mark
 
 
-def get_tree_style(tree: ToyTree, **kwargs) -> TreeStyle:
+def get_tree_style_base(tree: ToyTree, **kwargs) -> TreeStyle:
     """Return a style class object updated by user-args.
 
     If a `tree_style` arg was entered this overrides the default
     base style. Any additional style options in kwargs are applied
     on top of the base style.
     """
-    # update kwargs for optional shortname for 'treestyle'
-    if kwargs.get("ts"):
-        kwargs["tree_style"] = kwargs.pop("ts")
-
-    # get new TreeStyle from entered key (e.g., 's')
-    if kwargs.get("tree_style"):
-        style = get_base_tree_style_by_name(kwargs["tree_style"][0])
-    # get new TreeStyle from the key in .style (e.g., 'n')
-    elif tree.style.tree_style is not None:
-        style = get_base_tree_style_by_name(tree.style.tree_style)
-    # use existing TreeStyle in .style (no overwrite of base style)
+    # allow tree_style or ts as args.
+    if kwargs.get('tree_style'):
+        tree_style = kwargs['tree_style']
     else:
-        style = deepcopy(tree.style)
+        tree_style = kwargs.get('ts')
 
-    # update base style with user entered style kwargs
-    for key, value in kwargs.items():
+    # get new TreeStyle from user-entered arg (e.g., 's')
+    if tree_style is not None:
+        style = get_base_tree_style_by_name(tree_style[0])
 
-        # skip key if left at default value (None)
-        if value is None:
-            continue
+    # get new TreeStyle from tree.style.tree_style (e.g., 'n')
+    elif tree.style.tree_style is not None:
+        style = get_base_tree_style_by_name(tree.style.tree_style[0])
 
-        # check if it is a substyledict
-        substyle = getattr(style, key, "skip")
-
-        # skip non TreeStyle kwargs (e.g., fixed_order, fixed_position)
-        if substyle == "skip":
-            continue
-
-        # update value of a standard style argument
-        if not isinstance(substyle, SubStyle):
-            setattr(style, key, value)
-
-        # update a substyle dict
-        else:
-            for sub_key in value:
-                sub_value = value[sub_key]
-                sub_key = sub_key.replace("-", "_")  # for -toyplot-anchor-shift, etc.
-                if hasattr(substyle, sub_key):
-                    setattr(substyle, sub_key, sub_value)
-                else:
-                    logger.warning(
-                        f"Unrecognized substyle drawing arg skipped: {sub_key} ({substyle})."
-                    )
+    # get deepcopy of current TreeStyle in tree.style
+    else:
+        style = tree.style.copy()
     return style
 
 
@@ -156,8 +139,10 @@ def get_layout(tree: ToyTree, style: TreeStyle, **kwargs) -> BaseLayout:
     # return a linear layout
     if style.layout in "rlud":
         return LinearLayout(
-            tree, style,
-            kwargs.get("fixed_order"), kwargs.get("fixed_position")
+            tree,
+            style,
+            kwargs.get("fixed_order"),
+            kwargs.get("fixed_position")
         )
 
     # warn user that positional constraints are only for linear layouts
@@ -181,4 +166,7 @@ if __name__ == "__main__":
     import toytree
     toytree.set_log_level("DEBUG")
     tre = toytree.rtree.unittree(10)
-    tre._draw_browser(edge_style={"stroke-width": 5, "stroke-opacity": 0.7})
+    tre._draw_browser(
+        height=400, width=400,
+        edge_style={"stroke-width": 5, "stroke-opacity": 0.7},
+    )
