d86c667d51076ec6e5d7fd3e7446cd40dc477a5a
Deren <de2356@columbia.edu>
2023-04-30 23:05:41 -0400

    use new get_canvas_and_axes

--- toytree/drawing/__init__.py
+++ toytree/drawing/__init__.py
@@ -6,11 +6,7 @@
 
 from toytree.drawing.src.toytree_mark import ToyTreeMark
 from toytree.drawing.src.setup_grid import GridSetup
-from toytree.drawing.src.setup_canvas import (
-    get_canvas_and_axes,
-    set_axes_ticks_style,
-    CanvasSetup,
-)
+from toytree.drawing.src.setup_canvas import get_canvas_and_axes
 from toytree.drawing.src.draw_toytree import (
     draw_toytree,
     get_layout,
