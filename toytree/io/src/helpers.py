2cd551674684b94e8532090d1e4dfbc697c8b2d3
Deren <de2356@columbia.edu>
2023-06-03 16:10:48 -0400

    helpers is deprecated given treeio

--- toytree/io/src/helpers.py
+++ toytree/io/src/helpers.py
@@ -12,33 +12,34 @@ x- toytree.io.parse_tree_from_bpp_file
 x- toytree.io.parse_tree_from_superbpp_file
 """
 
-from typing import Union, TypeVar, Optional
+from typing import Union, TypeVar, Optional, Union
 import re
-import toytree
-from toytree.io.src.parse import TreeIOParser
+from pathlib import Path
+from toytree.core import ToyTree
+from toytree.io.src.parse import parse_tree, parse_tree_object
 from toytree.utils import ToytreeError
 
 # temporary
-ToyTree = TypeVar("ToyTree")
+# ToyTree = TypeVar("ToyTree")
 MultiTree = TypeVar("MultiTree")
 
 
 def read_newick(
-    path: str,
+    path: Union[str, Path],
     feature_prefix: str = "&",
-    feature_delim: str = ",", 
+    feature_delim: str = ",",
     feature_assignment: str = "=",
     internal_labels: Optional[str] = None,
-    ) -> Union[ToyTree, MultiTree]:
+) -> Union[ToyTree, MultiTree]:
     """Return a ToyTree or MultiTree from a newick file.
 
-    The file should contain one or more newick tree strings each on 
-    a separate line. This will parse the newick strings including 
-    those in extended-newick format with data stored in square-bracket 
+    The file should contain one or more newick tree strings each on
+    a separate line. This will parse the newick strings including
+    those in extended-newick format with data stored in square-bracket
     comment blocks and return ToyTrees with data stored to Nodes.
 
-    Compared to `toytree.tree` this function has additional options 
-    for specifying the format of feature comment blocks, and of the 
+    Compared to `toytree.tree` this function has additional options
+    for specifying the format of feature comment blocks, and of the
     internal node label types (e.g., support values or names).
 
     See Also
@@ -54,23 +55,23 @@ def read_newick(
         your file if you are unsure.
     feature_delim: str
         The character used to delimit features inside of a comment
-        block. This is usually "," or ":". 
+        block. This is usually "," or ":".
     feature_assignment: str
         The character separating feature names and values in a comment
-        block used for assignment. This is usually "=" or ":". 
+        block used for assignment. This is usually "=" or ":".
     internal_labels: str or None
         The feature that is present on internal node labels. If None
         then it will be inferred from the values present. Internal
-        labels are usually either 'support' or 'name'. If only numeric 
-        values are present then it is parsed as 'support' floats, 
+        labels are usually either 'support' or 'name'. If only numeric
+        values are present then it is parsed as 'support' floats,
         but this can overridden here if set `internal_labels='name'`.
 
     Examples
     --------
     >>> nwk1 = "((A,B),(C,D));"
     >>> tree1 = toytree.io.read_newick(nwk1)
-    >>> print(tree1.get_node_data())    
-    >>> 
+    >>> print(tree1.get_node_data())
+    >>>
     >>> nwk2 = "((a:1,b:2)0.99:3,(c:1,d:1)0.90:3)0.66:1;"
     >>> tree2 = toytree.io.read_newick(nwk2)
     >>> print(tree2.get_node_data())
@@ -78,35 +79,35 @@ def read_newick(
     kwargs = {
         "feature_prefix": feature_prefix,
         "feature_delim": feature_delim,
-        "feature_assignment": feature_assignment, 
+        "feature_assignment": feature_assignment,
         "internal_labels": internal_labels,
     }
-    treeio = TreeIOParser(path, **kwargs)
-    return treeio.parse_tree_from_file()
+    return parse_tree(path, **kwargs)
 
-def read_nexus(**kwargs):
+
+def read_nexus(path: Union[str, Path], **kwargs) -> ToyTree:
     """Return a ToyTree or MultiTree from a NEXUS file.
 
     The NEXUS file must contain a 'trees' block that includes one
-    or more trees. This function takes the same arguments as 
+    or more trees. This function takes the same arguments as
     `read_newick` for parsing extended-newick format of newick strings.
     It has additional arguments to parse other data from the NEXUS
     file, such as from the ... block (TODO)...
     """
-    return read_newick(**kwargs)
+    return parse_tree_object(path, **kwargs)
 
 
-def read_mb_file(path: str, **kwargs) -> Union[ToyTree, MultiTree]:
+def read_mb_file(path: Union[str, Path], **kwargs) -> Union[ToyTree, MultiTree]:
     """Return a ToyTree from a mrbayes tree file.
-    
+
     Mrbayes .trees files are NEXUS format with trees recorded
-    using an extended newick format (NHX-like) with comment blocks 
+    using an extended newick format (NHX-like) with comment blocks
     that include both edge and node statistics separately. A trees
     file may contain many trees (a posterior distribution) or a single
     tree (e.g., a consensus tree).
 
-    Internal node values can include ranges with "," inside, which 
-    requires additional comma-parsing that is automated by this 
+    Internal node values can include ranges with "," inside, which
+    requires additional comma-parsing that is automated by this
     function, as opposed to using `read_nexus`. TODO.
 
     Returns
@@ -114,36 +115,37 @@ def read_mb_file(path: str, **kwargs) -> Union[ToyTree, MultiTree]:
     ToyTree or MultiTree
         The returned type depends on whether one or more trees in file.
     """
-    kwargs['path'] = path
-    return toytree.io.read_nexus(**kwargs)
+    return parse_tree_object(path, **kwargs)
 
 
 def read_bpp_file(path: str, **kwargs):
     """Return a ToyTree or MultiTree from a BPP "00" tree file.
 
+    This format has NHX data separately on nodes and edges, which is
+    not typical other than perhaps for FigTree.
+
+    Example
+    -------
+    >>> (a,(b,c))[]
+    (6:0.000898,(5:0.000420,(3:0.000071,(2:0.000061,1:0.000061)[&height_95%_HPD={0.00004600,0.00007400},theta=0.0003823]:0.000010)[&height_95%_HPD={0.00005500,0.00008600},theta=0.0008018]:0.000349)[&height_95%_HPD={0.00033100,0.00050700},theta=0.0102051]:0.000478)[&height_95%_HPD={0.00078400,0.00101700},theta=0.0064148]
     """
-    # Converts bpp outfile format to normal newick
-    regex1 = re.compile(r" #[-+]?[0-9]*\.?[0-9]*[:]")
-    regex2 = re.compile(r" #[-+]?[0-9]*\.?[0-9]*[;]")
-    regex3 = re.compile(r": ")
-    new = regex1.sub(":", path)
-    new = regex2.sub(";", new)
-    new = regex3.sub(":", new)
-    new = new.strip()
-    # TODO
-    raise NotImplementedError("TODO...")
+    return parse_tree_object(path, **kwargs)
 
 
 if __name__ == "__main__":
-    
+
     # get a tree with metadata features
     import toytree
     TREE = toytree.rtree.imbtree(4, treeheight=3)
 
     # write/read newick
-    NWK = TREE.write()
-    TRE = toytree.io.read_newick(NWK)
-    print(TRE.get_node_data())
+    # NWK = TREE.write()
+    # TRE = toytree.io.read_newick(NWK)
+    # print(TRE.get_node_data())
+
+    # Example usage
+    newick_string = "(c,(a,b)[&x=3,y=2]:0.1)[&x=4,y=5]:0.5;"
 
     # write/read mrbayes NEXUS
-    pass
+    # BPP1 = "/home/deren/Downloads/bpp-cacti-res/clades5-nloci1K-sample500K_r3.figtree.nex"
+    # read_nexus(BPP1)
