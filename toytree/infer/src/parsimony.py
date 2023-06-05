4bad07da1ed1f06c93b7fd44332fb5f49e1b5c0c
Deren <de2356@columbia.edu>
2023-06-05 12:48:48 -0400

    cosmetic

--- toytree/infer/src/parsimony.py
+++ toytree/infer/src/parsimony.py
@@ -16,15 +16,14 @@ a new numba typed.set, but which does not yet exist. So until it does
 its not really worth spending time on...
 - https://numba.pydata.org/numba-doc/dev/reference/pysupported.html#set
 
-
 References
 ----------
-- Xia, Xuhua. 2018. “Maximum Parsimony Method in Phylogenetics.” 
-  In Bioinformatics and the Cell: Modern Computational Approaches in 
-  Genomics, Proteomics and Transcriptomics, edited by Xuhua Xia, 
-  327–41. Cham: Springer International Publishing. 
+- Xia, Xuhua. 2018. “Maximum Parsimony Method in Phylogenetics.”
+  In Bioinformatics and the Cell: Modern Computational Approaches in
+  Genomics, Proteomics and Transcriptomics, edited by Xuhua Xia,
+  327–41. Cham: Springer International Publishing.
   https://doi.org/10.1007/978-3-319-90684-3_14.
-- Fitch, Walter M. 1971. “Toward Defining the Course of Evolution: 
+- Fitch, Walter M. 1971. “Toward Defining the Course of Evolution:
   Minimum Change for a Specific Tree Topology.” Systematic Biology 20
   (4): 406–16. https://doi.org/10.1093/sysbio/20.4.406.
 
@@ -35,7 +34,7 @@ References
 - https://telliott99.blogspot.com/2010/03/fitch-and-sankoff-algorithms-for.html
 """
 
-from typing import TypeVar
+from typing import TypeVar, Optional
 from numpy.typing import ArrayLike
 
 ToyTree = TypeVar("ToyTree")
@@ -46,17 +45,17 @@ def get_parsimony_score(
     data: ArrayLike,
     weights: Optional[ArrayLike],
     data_as_dna: bool = True,
-    ):
+):
     """Return the parsimony score of a tree given a data matrix.
 
-    The parsimony score is calculated by performing a post-order 
+    The parsimony score is calculated by performing a post-order
     traversal on a rooted tree (if unrooted it will be arbitrarily
-    rooted, which has no effect on score) and setting the state of 
-    internal nodes to be the set intersection of states at its 
+    rooted, which has no effect on score) and setting the state of
+    internal nodes to be the set intersection of states at its
     descendant Nodes. If no intersection exists, the union is instead
-    set and this counts as a 'change' event. If a weights matrix is 
-    entered then this indicates the contribution of each type of 
-    change towards the 'score' (Sankoff parsimony), else each change 
+    set and this counts as a 'change' event. If a weights matrix is
+    entered then this indicates the contribution of each type of
+    change towards the 'score' (Sankoff parsimony), else each change
     counts as 1 (Fitch parsimony). If multiple states exist in the
     data matrix the sum of scores of each state is returned.
 
@@ -68,13 +67,13 @@ def get_parsimony_score(
         A data matrix of shape (ntips, ntraits) containing discrete
         values for one or more traits for each tip Node in idxorder.
     weights: None or ArrayLike
-        The weights matrix must be square. If it is a dataframe then 
-        the row and column names will be used, else they should be 
+        The weights matrix must be square. If it is a dataframe then
+        the row and column names will be used, else they should be
         ordered by state values alphanumerically (e.g., 0, 1, 2, 3 or
-        'A', 'C', 'G', 'T'). The diagonal must be zero, and 
+        'A', 'C', 'G', 'T'). The diagonal must be zero, and
         off-diagonal as float or int types.
     data_as_dna: bool
-        If True then data values that are string types representing 
+        If True then data values that are string types representing
         DNA IUPAC ambiguity codes (e.g., RWMYSK) will be expanded to a
         set of the two bases that they represent (e.g., W -> {A, T}).
 
@@ -108,17 +107,12 @@ def get_parsimony_score(
     return score
 
 
-
-
-
-
-
 class Parsimony:
     """Infer a phylogenetic tree by Maximum Parsimony.
 
     Note
     ----
-    The maximum parsimony algorithm does not infer rooted trees, i.e., 
+    The maximum parsimony algorithm does not infer rooted trees, i.e.,
     topologies re-rooted at any arbitrary edge will yield the same
     parsimony score.
 
@@ -130,7 +124,7 @@ class Parsimony:
     >>> tool = Parsimony(data)
     >>> tool.get_score(tree=tree, )
     """
-    def __init__(self, data: ):
+    def __init__(self, data: ...):
         self.data = data
 
     def get_score(self, tree):
@@ -145,7 +139,7 @@ class Parsimony:
     def _sankoff_algorithm(self):
         """Implement the Sankoff algorithm.
 
-        The strength of the Sankoff algorithm is that it allows a 
+        The strength of the Sankoff algorithm is that it allows a
         variety of cost matrices to be used. This is in principal
         closer to ML, where we would define a substitution model.
         Here the cost matrix is not inferred, but a priori defined
