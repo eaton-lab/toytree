871076802590e5d8e06f0f9d35ae2e88d5bf9f91
Deren <de2356@columbia.edu>
2023-06-06 14:26:15 -0400

    writes node and edge features separately.

--- toytree/io/src/writer.py
+++ toytree/io/src/writer.py
@@ -20,7 +20,7 @@ from toytree.utils import ToytreeError
 
 Node = TypeVar("Node")
 ToyTree = TypeVar("ToyTree")
-DISALLOWED_FEATURES = set(['idx', 'dist', 'support', 'up', 'children', 'height'])
+DISALLOWED_FEATURES = set(['idx', 'dist', 'up', 'children'])  # 'support', 'height'])
 logger = logger.bind(name="toytree")
 
 
@@ -41,9 +41,14 @@ def get_feature_string(
         return ""
     pairs = []
     for feature in features:
-        # TODO: how to handle mixed presence/absence or types of features?
+        # get value or None if Node does not have the feature
         value = getattr(node, feature, None)
-        # try to format float value to a string
+
+        # if no value then do not write it
+        if value is None:
+            continue
+
+        # try to float format the value to a string
         try:
             if np.isnan(value):
                 value = ""
@@ -51,8 +56,10 @@ def get_feature_string(
                 value = internal_labels_formatter % str(value)
         except TypeError:
             value = str(value)
-            pairs.append(features_assignment.join((feature, value)))
+        pairs.append(features_assignment.join((feature, value)))
     feature_str = features_delim.join(pairs)
+    if not feature_str:
+        return ""
     return f"[{features_prefix}{feature_str}]"
 
 
@@ -62,16 +69,30 @@ def node_to_newick(
     dist_formatter: str = "%.12g",
     internal_labels: Optional[str] = "support",
     internal_labels_formatter: Optional[str] = "%.12g",
-    features: Optional[Sequence[str]] = None,
+    node_features: Optional[Sequence[str]] = None,
+    edge_features: Optional[Sequence[str]] = None,
     features_prefix: str = "&",
     features_delim: str = ",",
     features_assignment: str = "=",
 ) -> str:
     """Reduce function used in tree_reduce"""
     # format the comment feature string for extra features
-    feature_str = get_feature_string(
-        node, features, features_prefix, features_delim,
-        features_assignment, internal_labels_formatter)
+    node_feature_str = get_feature_string(
+        node,
+        node_features,
+        features_prefix,
+        features_delim,
+        features_assignment,
+        internal_labels_formatter,
+    )
+    edge_feature_str = get_feature_string(
+        node,
+        edge_features,
+        features_prefix,
+        features_delim,
+        features_assignment,
+        internal_labels_formatter,
+    )
 
     # format the dist values (edge lengths) as strings
     dist = "" if dist_formatter is None else ":" + dist_formatter % node.dist
@@ -92,13 +113,18 @@ def node_to_newick(
             except TypeError:
                 internal = str(internal)
 
-    # return the node formatted as newick
+    # tip node
     if node.is_leaf():
-        return f"{node.name}{dist}{feature_str}"
-    # do not write dist value for the root, it is not really part of the data.
-    elif node.is_root():
-        return f"({','.join(children)}){internal}{feature_str}"
-    return f"({','.join(children)}){internal}{dist}{feature_str}"
+        return f"{node.name}{node_feature_str}{dist}{edge_feature_str}"
+
+    # root node
+    if node.is_root():
+        if node.dist:
+            return f"({','.join(children)}){internal}{node_feature_str}{dist}{edge_feature_str}"
+        return f"({','.join(children)}){internal}{node_feature_str}"
+
+    # other internal nodes
+    return f"({','.join(children)}){internal}{node_feature_str}{dist}{edge_feature_str}"
 
 
 def tree_reduce(
@@ -106,7 +132,8 @@ def tree_reduce(
     dist_formatter: str = "%.12g",
     internal_labels: Optional[str] = "support",
     internal_labels_formatter: Optional[str] = "%.12g",
-    features: Optional[Sequence[str]] = None,
+    node_features: Optional[Sequence[str]] = None,
+    edge_features: Optional[Sequence[str]] = None,
     features_prefix: str = "&",
     features_delim: str = ",",
     features_assignment: str = "=",
@@ -118,7 +145,8 @@ def tree_reduce(
     """
     args = [
         dist_formatter, internal_labels, internal_labels_formatter,
-        features, features_prefix, features_delim, features_assignment,
+        node_features, edge_features,
+        features_prefix, features_delim, features_assignment,
     ]
     reduced_children = [tree_reduce(child, *args) for child in node.children]
     newick = node_to_newick(node, reduced_children, *args)
@@ -171,6 +199,9 @@ def write_newick(
     features: List[str]
         A list of additional features to write in the newick comment
         block. For example, features=["height"] will save heights.
+        Features that are in `.edge_features` will be written as NHX
+        meta data for edges, while other features will be written as
+        NHX meta data for nodes.
     features_prefix: str
         A prefix character written to the start of newick comment
         blocks. Typical values are "&" (default) or "&&NHX:".
@@ -190,32 +221,48 @@ def write_newick(
 
     Examples
     --------
-    >>> nwk = "((a:3[&state=1],b:3[&state=1])D:1[&state=1],c:4[&state=2])E:1[&state=1];"
-    >>> tree = toytree.io.parse_newick(nwk, features_prefix="&")
-    >>> tree.write()
-    >>> # ((a:3,b:3)100:1,c:4)100:1
-    >>> tree.write(dist_formatter=None)
-    >>> # ((a,b)100,c)100
-    >>> tree.write(internal_label=None)
-    >>> # ((a:3,b:3):1,c:4):1
-    >>> tree.write(internal_labels="name")
-    >>> # ((a:3,b:3)D:1,c:4)E:1
-    >>> tree.write(features=["size"])
-    >>> # ((a:3[&state=1],b:3[&state=1])100:1[&state=1],c:4[&state=2])100:1[&state=1]
+    >>> # example tree w/ support on all Nodes and X values on few Nodes
+    >>> tree0 = toytree.rtree.unittree(4, seed=123)
+    >>> tree0.set_node_data("support", default=100, inplace=True)
+    >>> tree0.set_node_data("X", {"r0": 3, "r1": 4}, inplace=True)
+
+    >>> # default arguments to write
+    >>> tree0.write()
+    >>> # '((r0:0.5,r1:0.5)100:0.5,(r2:0.5,r3:0.5)100:0.5)100;'
+
+    >>> # write topology only
+    >>> tree0.write(None, None, None)
+    >>> # '(((r0,r1),r2),(r3,r4));'
+
+    >>> # write internal labels (in this case 'support') only
+    >>> tree0.write(path=None, dist_formatter=None, internal_labels="support")
+    >>> # '((r0,r1)100,(r2,r3)100)100;'
+
+    >>> # write branch lengths (dist values) only
+    >>> tree0.write(path=None, dist_formatter="%.12g", internal_labels=None)
+    >>> # '((r0:0.5,r1:0.5):0.5,(r2:0.5,r3:0.5):0.5);'
+
+    >>> # write NHX format meta data (support and X features)
+    >>> tree0.write(None, None, None, features=["support", "X"])
     """
-    if features:
-        features = [features] if isinstance(features, str) else features
-        features = set(features) - DISALLOWED_FEATURES
-        bad_features = features - set(tree.features)
-        if bad_features:
-            raise ToytreeError(
-                f"Cannot write features not present in tree: {bad_features}")
+    if features is None:
+        features = []
+    features = [features] if isinstance(features, str) else features
+    features = set(features) - DISALLOWED_FEATURES
+    bad_features = features - set(tree.features)
+    if bad_features:
+        raise ToytreeError(
+            f"Cannot write features not present in tree: {bad_features}")
+    node_features = {i for i in features if i not in tree.edge_features}
+    edge_features = features - node_features
+
     newick = tree_reduce(
         tree.treenode,
         dist_formatter,
         internal_labels,
         internal_labels_formatter,
-        features,
+        node_features,
+        edge_features,
         features_prefix,
         features_delim,
         features_assignment,
@@ -253,8 +300,8 @@ if __name__ == "__main__":
 
     import toytree
     NWK = "((a:3[&state=1],b:3[&state=1])D:1[&state=1],c:4[&state=2])E:1[&state=1];"
-    TREE = toytree.io.parse_newick_string(NWK)
-    TREE = TREE.set_node_data("support", default=100)
+    TREE = toytree.tree(NWK)
+    # TREE = TREE.set_node_data("support", default=100)
 
     print(write_newick(TREE))
     print(write_newick(TREE, dist_formatter=None))
@@ -262,3 +309,8 @@ if __name__ == "__main__":
     print(write_newick(TREE, internal_labels="name"))
     print(write_newick(TREE, features=["state"]))
     print(write_nexus(TREE, features=["state"]))
+
+    TREE.set_node_data('test', {'a': 7.8, 4: 5}, inplace=True)
+    TREE.set_node_data('support', default=100, inplace=True)
+    print(TREE.edge_features)
+    print(write_newick(TREE, internal_labels=None, features=["support", "test"]))
