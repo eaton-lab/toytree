7a4dee988427aed28427fe8f45a92160f038e3e6
Deren <de2356@columbia.edu>
2024-03-10 15:14:08 -0400

    added in prep io test

--- toytree/io/tests/test_parsing.py
+++ toytree/io/tests/test_parsing.py
@@ -5,7 +5,7 @@
 
 [ ] empty toytree.tree()
 [ ] unrooted newick with support for all internal except last two, if two internal nodes, else last 1 (note: if only 1 internal node on unrooted tree then no split exists)
-[ ] mrbayes
+[ ] mrbayes: "(Node)Name[&node_data=...]:Dist[&edge_data=...], ..."
 [ ] scientific notation read and write
 [ ] names with escape characters?
 """
