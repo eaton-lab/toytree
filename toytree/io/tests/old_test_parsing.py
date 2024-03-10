1c33f62547fe3e29f8b48ca8c031a17675c48cbb
Deren <de2356@columbia.edu>
2024-03-10 15:14:26 -0400

    added in prep io test

--- toytree/io/tests/old_test_parsing.py
+++ toytree/io/tests/old_test_parsing.py
@@ -8,7 +8,7 @@ and at sufficient speeds.
 
 # pylint: disable=missing-function-docstring
 
-import pytest
+# import pytest
 import toytree
 
 # @pytest.fixture
@@ -19,6 +19,22 @@ NEWICK_2 = "((a:2,b:2)A:2,c:2)B:2;"
 NEWICK_3 = "((a[&NHX:label=3],b[&NHX:label=4]),c[&NHX:label=5]);"
 NEWICK_4 = "((a:3[&NHX:name=3],b:4[&NHX:name=4]),c:5[&NHX:name=5]);"
 NEWICKS = [NEWICK_1, NEWICK_2]
+NEXUS_1 = """\
+#NEXUS
+begin mrbayes;
+    set autoclose=yes nowarn=yes;
+    execute my_data.nex;
+    lset nst=6 rates=invgamma;
+    mcmc ngen=1000000 samplefreq=1000 printfreq=10000;
+end;
+
+begin trees;
+    tree Tree1 = (A,(B,C));
+    tree Tree2 = ((A,B),C);
+    tree Tree3 = ((A,C),B);
+end;
+"""
+
 
 def test_parse_newick():
     tree = toytree.tree(NEWICK_2)
@@ -27,15 +43,18 @@ def test_parse_newick():
     assert all(tree.get_node_data("dist") == 2.)
     assert list(tree.get_node_data("name")[-2:]) == ['A', 'B']
 
+
 def test_no_names_newick():
     tree = toytree.tree(NEWICK_1)
     assert tree.nnodes == 8
     assert tree.ntips == 5
 
+
 def test_parse_newick_nhx():
     tree = toytree.tree(NEWICK_3)
     assert tree.nnodes == 5
     assert tree.ntips == 3
+
 
 def test_parse_newick_2():
     tree = toytree.io.parse_newick_string(
@@ -46,9 +65,11 @@ def test_parse_newick_2():
     assert tree.nnodes == 5
     assert tree.ntips == 3
 
+
 def test_parse_url():
     tree = toytree.tree("https://eaton-lab.org/data/Cyathophora.tre")
     assert tree.ntips == 13
+
 
 def test_parse_nexus():
     pass
