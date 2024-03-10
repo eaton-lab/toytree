cff43d88488b3f9e545b58846111bf16b828784c
Deren <de2356@columbia.edu>
2024-03-10 15:20:43 -0400

    docstring

--- toytree/distance/tests/test_tree_dist.py
+++ toytree/distance/tests/test_tree_dist.py
@@ -2,9 +2,9 @@
 
 """Test tree dist statistics against results calculated in R.
 
-This file just tests against stored results so that tests do not 
+This file just tests against stored results so that tests do not
 require an R installation, but the special test file XXX can be
 used to generate new results in R:::TreeDist for comparison.
 """
 
-
+# TODO...
