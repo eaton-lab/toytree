e6440fe2fd7892e6bd3673b6415d270ada9bc25a
natetrux <natetrux@gmail.com>
2024-07-11 13:32:20 -0400

    added tests for sorting methodsin quartets.py

--- toytree/enum/tests/test_quartets.py
+++ toytree/enum/tests/test_quartets.py
@@ -18,6 +18,7 @@ class TestQuartets(unittest.TestCase):
         self.tree2 = toytree.rtree.unittree(6, seed=123, random_names=True)
         self.trees = [self.tree1, self.tree2]
     def test_iter_quartet_sets(self):
+        #feature = 'name', quadripartitions = True
         QUADRIPARTITION_PARTS = [('c', 'd', 'e', 'a'),
                             ('c', 'd', 'e', 'b'),
                             ('c', 'd', 'f', 'a'),
@@ -33,6 +34,7 @@ class TestQuartets(unittest.TestCase):
         quadripartition_parts = sorted(_iter_quartet_sets(self.tree1, feature = 'name', quadripartitions = True))
         self.assertEqual(quadripartition_parts, QUADRIPARTITION_PARTS)
     def test_iter_unresolved_quartet_sets(self):
+        #feature = 'name'
         PARTS = [{'a', 'b', 'c', 'd'},
                 {'a', 'b', 'c', 'e'},
                 {'a', 'b', 'c', 'f'},
@@ -52,6 +54,7 @@ class TestQuartets(unittest.TestCase):
         self.assertEqual(parts, PARTS)
     def test_iter_quartets(self):
         """Quartets"""
+        #type=tuple, collapse=False, sort=True
         PARTS = [(('r0', 'r1'), ('r2', 'r3')), 
                   (('r0', 'r1'), ('r2', 'r4')), 
                   (('r0', 'r1'), ('r2', 'r5')), 
@@ -70,7 +73,7 @@ class TestQuartets(unittest.TestCase):
         
         parts = sorted(iter_quartets(self.tree2, type=tuple, collapse=False, sort=True))
         self.assertEqual(parts, PARTS)
-        
+        #type=tuple, collapse=True, sort=True, quadripartitions=True
         PARTS = [('r0', 'r1', 'r2', 'r4'), 
                   ('r0', 'r1', 'r2', 'r5'), 
                   ('r0', 'r1', 'r3', 'r4'), 
@@ -85,7 +88,65 @@ class TestQuartets(unittest.TestCase):
                   ('r1', 'r5', 'r2', 'r3')]
         parts = sorted(iter_quartets(self.tree2, type=tuple, collapse=True, sort=True, quadripartitions=True))
         self.assertEqual(parts, PARTS)
-        
+        #type=list, collapse=False, sort=True
+        PARTS = [(['r0', 'r1'], ['r2', 'r3']), 
+                 (['r0', 'r1'], ['r2', 'r4']), 
+                 (['r0', 'r1'], ['r2', 'r5']), 
+                 (['r0', 'r1'], ['r3', 'r4']), 
+                 (['r0', 'r1'], ['r3', 'r5']), 
+                 (['r0', 'r1'], ['r4', 'r5']), 
+                 (['r0', 'r2'], ['r4', 'r5']), 
+                 (['r0', 'r3'], ['r4', 'r5']), 
+                 (['r0', 'r4'], ['r2', 'r3']), 
+                 (['r0', 'r5'], ['r2', 'r3']), 
+                 (['r1', 'r2'], ['r4', 'r5']), 
+                 (['r1', 'r3'], ['r4', 'r5']), 
+                 (['r1', 'r4'], ['r2', 'r3']), 
+                 (['r1', 'r5'], ['r2', 'r3']), 
+                 (['r2', 'r3'], ['r4', 'r5'])]
+        parts = (sorted(iter_quartets(self.tree2, type=list, collapse=False, sort=True)))
+        self.assertEqual(parts, PARTS)
+        #type=list, collapse=True, sort=True
+        PARTS = [['r0', 'r1', 'r2', 'r3'], 
+                 ['r0', 'r1', 'r2', 'r4'], 
+                 ['r0', 'r1', 'r2', 'r5'], 
+                 ['r0', 'r1', 'r3', 'r4'], 
+                 ['r0', 'r1', 'r3', 'r5'], 
+                 ['r0', 'r1', 'r4', 'r5'], 
+                 ['r0', 'r2', 'r4', 'r5'], 
+                 ['r0', 'r3', 'r4', 'r5'], 
+                 ['r0', 'r4', 'r2', 'r3'], 
+                 ['r0', 'r5', 'r2', 'r3'], 
+                 ['r1', 'r2', 'r4', 'r5'], 
+                 ['r1', 'r3', 'r4', 'r5'], 
+                 ['r1', 'r4', 'r2', 'r3'], 
+                 ['r1', 'r5', 'r2', 'r3'], 
+                 ['r2', 'r3', 'r4', 'r5']]
+        parts = (sorted(iter_quartets(self.tree2, type=list, collapse=True, sort=True)))
+        self.assertEqual(parts, PARTS)
+
+        #testing sorting methods (both built in tuple-level sort and (sort=) arg collection-level sort)
+
+        #type=list, collapse=False, sort=True
+        for item in list(iter_quartets(self.tree2, type=list, collapse=False, sort=True)):
+            self.assertGreater(item[1][0], item[0][0]) #collection-level
+            for collection in item:
+                self.assertGreater(collection[1], collection[0]) #tuple-level
+        #type=tuple, collapse=False, sort=True
+        for item in list(iter_quartets(self.tree2, type=tuple, collapse=False, sort=True)):
+            self.assertGreater(item[1][0], item[0][0])
+            for collection in item:
+                self.assertGreater(collection[1], collection[0])
+        #type=list, collapse=True, sort=True
+        for item in list(iter_quartets(self.tree2, type=list, collapse=True, sort=True)):
+            self.assertGreater(item[2], item[0]) #collection-level
+            self.assertGreater(item[1], item[0]) #tuple-level
+            self.assertGreater(item[3], item[2])
+        #type=tuple, collapse=True, sort=True
+        for item in list(iter_quartets(self.tree2, type=tuple, collapse=True, sort=True)):
+            self.assertGreater(item[2], item[0]) #collection-level
+            self.assertGreater(item[1], item[0]) #tuple- level
+            self.assertGreater(item[3], item[2])
 
 
 
