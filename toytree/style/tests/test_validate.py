88e04f8dc700a77679b68801b5b4dac478986012
deren <de2356@columbia.edu>
2024-07-22 11:38:07 -0400

    style validation tests updated

--- toytree/style/tests/test_validate.py
+++ toytree/style/tests/test_validate.py
@@ -7,11 +7,11 @@
 
 
 
-validate_edge_widths(tree, 0)
-validate_edge_widths(tree, 1)
-validate_edge_widths(tree, 1.555)
-validate_edge_widths(tree, np.arange(tree.nnodes))
-validate_edge_widths(tree, ("dist", ))
-validate_edge_widths(tree, ("dist", 2))
-validate_edge_widths(tree, ("dist", 2, 4))
-validate_edge_widths(tree, ("dist", 2, 4, 0))
+# validate_edge_widths(tree, 0)
+# validate_edge_widths(tree, 1)
+# validate_edge_widths(tree, 1.555)
+# validate_edge_widths(tree, np.arange(tree.nnodes))
+# validate_edge_widths(tree, ("dist", ))
+# validate_edge_widths(tree, ("dist", 2))
+# validate_edge_widths(tree, ("dist", 2, 4))
+# validate_edge_widths(tree, ("dist", 2, 4, 0))
--- toytree/style/tests/test_validator.py
+++ toytree/style/tests/test_validator.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
-"""...
+"""Test validation of tree drawing style arguments in `toytree.style`
 
+TreeStyle is a DataClass with a serialized repr. It does not validate
+values during setting; instead, a validator is run instide of .draw
+when the tree is drawn.
 """
-
 
 import unittest
 import numpy as np
 import toyplot
 import toytree
-from toytree.style import TreeStyle, validate_style
-from toytree.style.src.color_mapper import get_color_mapped_feature
-from toytree.utils import ToytreeError
+from toytree.style import (
+    TreeStyle,
+    validate_style,
+    get_color_mapped_values,
+    get_color_mapped_feature,
+)
+# from toytree.utils import ToytreeError
+
+class Devnull(object):
+    """Fake stream to test repr"""
+    def write(self, *_): pass
 
 
 class TestValidator(unittest.TestCase):
     def setUp(self):
-
         self.itree = toytree.rtree.imbtree(10, seed=123, treeheight=10)
         self.btree = toytree.rtree.baltree(10, seed=123, treeheight=10)
         self.utree = toytree.rtree.unittree(10, seed=123, treeheight=10)
@@ -35,30 +44,26 @@ class TestValidator(unittest.TestCase):
         ]
         for color_arg in colors:
             tree.style.node_colors = color_arg
-            tree.style.json()
+            print(tree.style, file=Devnull)
 
     def test_node_colormap_support_nan(self):
         """Set nan values to transparent when color mapping."""
-        # set data with missing values on some nodes
+        # only set some data, others have nan.
         svals = {11: 0, 12: 50, 13: 75, 14: 100}
         tree = self.itree.set_node_data("support", svals)
         vals = tree.get_node_data("support").values
-
-        # create a color map
-        cmap = toyplot.color.brewer.map("BlueRed")
-        cmap.domain.max = 0
-        cmap.domain.max = 10
-
-        # call internal color mapping func used by validator for (feat, cmap)
-        colors = get_color_mapped_feature(vals, cmap)
+        colors = toytree.style.get_color_mapped_feature(
+            tree=tree,
+            feature="support",
+            cmap="BlueRed",
+            domain_min=0,
+            domain_max=10,
+        )
 
         # assert nans were converted to transparent
         pre = np.where(np.isnan(vals))
         post = np.where(colors == toytree.color.ToyColor("transparent"))
         self.assertEqual(list(pre[0]), list(post[0]))
-
-        # assert no min max domain on colormap gets autosettings.
-        # ...
 
     def test_node_style_opacity_overrule(self):
         """... opacity should overrule orig fill color opacity."""
@@ -73,32 +78,31 @@ class TestValidator(unittest.TestCase):
         tree.style.node_style.fill_opacity = 0.2
         # self.assertEqual()        
 
-    def test_edge_style_opacity_overrule(self):
-        """..."""
-        tree = self.itree
-        tree.style.edge_colors = ['red'] + ['blue'] * (tree.nnodes - 1)
-        tree.style.edge_style.stroke_opacity = 0.2
-        style = tree.style.copy()
-        # validate_style(tree, style)
-        # print(style.node_colors)
+    # def test_edge_style_opacity_overrule_2(self):
+    #     """..."""
+    #     tree = self.itree
+    #     tree.style.edge_colors = ['red'] + ['blue'] * (tree.nnodes - 1)
+    #     tree.style.edge_style.stroke_opacity = 0.2
+    #     style = tree.style.copy()
+    #     # validate_style(tree, style)
+    #     # print(style.node_colors)
 
-    def test_set_nan_node_labels_empty_string(self):
-        """np.nan values on node labels should validate to empty strings."""
-        self.itree.style.node_colors = "red"
+    # def test_set_nan_node_labels_empty_string(self):
+    #     """np.nan values on node labels should validate to empty strings."""
+    #     self.itree.style.node_colors = "red"
         # style = self.itree.style._validate()
         # self.assertEqual(style, dict...)
 
     # def test_node_size_to_zeros_for_empty_labels(self):
     #     """Do this?"""
 
-    def test_node_labels_round_floats_on_validate(self):
-        """..."""
-        self.btree.treenode.children[0].support = 99.99
-        self.btree.treenode.children[1].support = 10
+    # def test_node_labels_round_floats_on_validate(self):
+    #     """..."""
+    #     self.btree.treenode.children[0].support = 99.99
+    #     self.btree.treenode.children[1].support = 10
 
-
-    def test_anchor_shift_legacy_support(self):
-        """Support -toyplot-anchor-shift as legacy to anchor-shift."""
+    # def test_anchor_shift_legacy_support(self):
+    #     """Support -toyplot-anchor-shift as legacy to anchor-shift."""
 
         # assert warning message is reported on -toyplot-anchor-shift
 
