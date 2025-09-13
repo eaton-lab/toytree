13bf56dcdcec4bd53d7e1d4f4b7183c398e8a3f3
deren <de2356@columbia.edu>
2025-09-13 13:45:39 -0400

    added placeholder

---
+++ toytree/pcm/src/phylocom/pglmm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""Phylogenetic general linear mixed models (PGLMMs)
+
+Table from Ives and Helmus 2011
+
+M | spp-prev | spp-occ | spp-sens | spp-repuls | spp-trait | site-rich | 
+-----------------------------------------------------------------------
+1 | fixed    | phylo   |    -     |      -     |     -     |    R      |
+2 | fixed    |   -     |   P,R    |      -     |     -     |    R      |
+...
+
+"""
