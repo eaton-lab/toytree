3200d4ef48d62f33d36e0ee8efa74e9bdf564e71
deren <de2356@columbia.edu>
2026-02-24 14:30:33 -0500

    relabel method

--- toytree/data/__init__.py
+++ toytree/data/__init__.py
@@ -17,5 +17,6 @@ toytree.data.sequence_alignment
 # methods here will be available at submodule-level (toytree.data.[method])
 from ._src.get_node_data import get_node_data
 from ._src.set_node_data import set_node_data
+from ._src.relabel import relabel
 from ._src.expand_node_mapping import expand_node_mapping
 # from toytree.data._src.transform import normalize_values
--- toytree/data/_src/__init__.py
+++ toytree/data/_src/__init__.py
@@ -8,3 +8,4 @@
 # and should have the @add_toytree_method decorator
 from .get_node_data import get_node_data
 from .set_node_data import set_node_data
+from .relabel import relabel
