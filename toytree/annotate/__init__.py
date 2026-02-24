d1f0e6bc0da100d2c2cb371da231401d1f258560
deren <de2356@columbia.edu>
2026-02-24 14:29:04 -0500

    add edges

--- toytree/annotate/__init__.py
+++ toytree/annotate/__init__.py
@@ -45,6 +45,7 @@ Functions
 
 # from toytree.annotate.src.node_pie_charts import draw_node_pie_charts
 from toytree.annotate.src.add_edge_markers import *
+from toytree.annotate.src.add_edges import *
 from toytree.annotate.src.add_node_markers import *
 from toytree.annotate.src.add_tip_markers import *
 from toytree.annotate.src.add_axes_box_outline import *
