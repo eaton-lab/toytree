06c8d2b07ff365b3fd1e9d0464b833f61af0d6ab
deren <de2356@columbia.edu>
2026-02-24 15:24:51 -0500

    connect new modules

--- toytree/drawing/src/render/__init__.py
+++ toytree/drawing/src/render/__init__.py
@@ -5,3 +5,5 @@ from . import render_pie
 from . import render_marker
 from . import render_annotation
 from . import render_text         # check this...
+from . import render_axis
+from . import svg_defs
